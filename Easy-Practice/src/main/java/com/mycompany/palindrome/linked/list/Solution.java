b1ac92c67f89e4c9b850543ad31060dbcf4fd6dd
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-12 17:43:22 -0500

    Created Classes

---
+++ Easy-Practice/src/main/java/com/mycompany/palindrome/linked/list/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.palindrome.linked.list;
+
+/**
+ *
+ * @author Rs
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head){
+        return false;
+    }
+}
