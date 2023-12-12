44e3d0432619508cd3fe4062ca3f7c9b649bbd55
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-12 17:41:57 -0500

    Created method to create a linked list by passing any set of integers

--- Easy-Practice/src/main/java/com/mycompany/palindrome/linked/list/Controller.java
+++ Easy-Practice/src/main/java/com/mycompany/palindrome/linked/list/Controller.java
@@ -10,6 +10,34 @@ package com.mycompany.palindrome.linked.list;
  */
 public class Controller {
     public static void main(String[] args) {
+        ListNode head1 = setNodes(1,2,3,4);
         
+        System.out.println(head1.val);
+        System.out.println(head1.next.val);
+        System.out.println(head1.next.next.val);
+        System.out.println(head1.next.next.next.val);
+        
+    }
+    
+    public static ListNode setNodes(int... numbers){
+        System.out.println("Length: " + numbers.length);
+        int count = 1;
+        ListNode head = new ListNode(numbers[0]);
+        ListNode current = new ListNode();
+        if(numbers.length != 1){head.next = current;} 
+        
+        for(int number : numbers){
+            if(count != 1) {
+                current.val = number; 
+                if(count < numbers.length){
+                    ListNode nextNode = new ListNode();
+                    current.next = nextNode;
+                    current = nextNode;
+                }
+            }
+            count++;
+        }
+        
+        return head;
     }
 }
