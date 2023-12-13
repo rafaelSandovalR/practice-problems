9be3b335258f61371f768b6d7317c3dcb6ccc6d3
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-13 15:41:21 -0500

    Changed package name

---
+++ Easy-Practice/src/main/java/palindrome_linkedList/Solution.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package palindrome_linkedList;
+
+/**
+ *
+ * @author Rs
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head){
+        ListNode slow=head, fast=head, prev, temp;
+        
+ //catch one or two length lists
+        if(head.next == null){return true; }
+        if(head.next.next == null){
+            return (head.val == head.next.val); 
+        }
+        
+ //traverse linked list to find midpoint
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+ //set middle node's next to null
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+ //invert second half of linked list
+        while(slow.next != null){
+            temp = prev;
+            prev = slow;
+            slow = slow.next;
+            prev.next = temp;
+        }
+        
+        slow.next = prev;
+        
+ //compare list from head & tail inward to determine if palindrome
+        fast = head;
+        while (slow != null){
+            if (fast.val != slow.val){return false; }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        return true;
+    }
+}
