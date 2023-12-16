a8176696320ffad2dca0fa22e8dd12d1cc614b7c
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-16 13:48:27 -0500

    Created hashmap and loop to fill it with the characters in the magazine variable and their count.

--- Easy-Practice/src/main/java/ransom_note/Solution.java
+++ Easy-Practice/src/main/java/ransom_note/Solution.java
@@ -4,12 +4,33 @@
  */
 package ransom_note;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Rsand
  */
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine){
+            //hashmap of letter and their count
+            Map<Character, Integer> m = new HashMap<>();
+            char letter;
             
+            for(int i = magazine.length()-1; i >= 0; i--){
+                letter = magazine.charAt(i);
+                
+                //if letter exists in the map, increment count
+                if(m.containsKey(letter)){
+                    m.compute(letter, (key, val) -> val = val+1);
+                } else {m.put(letter, 1);}
+            }
+            
+            //test to display count of all letter in map
+            m.forEach((key, value) -> {
+                System.out.println(key + ": " + value);
+            });
+        
+            return false;
     }
 }
