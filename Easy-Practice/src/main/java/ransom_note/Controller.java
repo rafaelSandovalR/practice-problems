2811946c5e525da4ac98127cc48522f0228dc41b
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-16 13:22:52 -0500

    NEW PROBLEM: Ransom Note Created classes & instruction file.

---
+++ Easy-Practice/src/main/java/ransom_note/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ransom_note;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        
+        
+    }
+}
---
+++ Easy-Practice/src/main/java/ransom_note/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ransom_note;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine){
+            
+    }
+}
