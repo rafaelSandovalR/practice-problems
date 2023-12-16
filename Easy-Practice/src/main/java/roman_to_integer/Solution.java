491b2716daa476922cab4a600bd45c9635c6b849
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-16 13:16:54 -0500

    Created a third solution that is more concise, quicker, and uses less memory by: having a switch statement, traversing backwards, having a variable to hold previous number.

--- Easy-Practice/src/main/java/roman_to_integer/Solution.java
+++ Easy-Practice/src/main/java/roman_to_integer/Solution.java
@@ -85,6 +85,31 @@ public class Solution {
      
      return output;
     }
+    
+    public int romanToInt3(String s){
+        //having a variable that holds the previous number takes up less time.
+        int ans = 0, num = 0, prev = 0;
+        
+        //iterate through the string in reverse. Easier for different lengths.
+        for(int i = s.length()-1; i >= 0; i--){
+            switch(s.charAt(i)){
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+            if(num < prev){ ans -= num; }
+            else { ans += num;}
+
+            prev = num;
+            
+        }
+       
+        return ans;
+    }
 }
 
 
