29ad45d85e7489e8305dd20e3ef0f405a95493ba
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-21 17:22:36 -0500

    NEW PROBLEM: Middle of the Linked List Created classes and instruction file

---
+++ Easy-Practice/src/main/java/middle_of_linked_list/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package middle_of_linked_list;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public ListNode middleNode(ListNode head){
+        
+    }
+}
