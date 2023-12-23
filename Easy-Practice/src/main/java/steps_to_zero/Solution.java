5d956e838c1edd990e2d0232a460609d3d21f9d5
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-23 12:50:14 -0500

    NEW PROBLEMS: Number of Steps to Reduce a Number to Zero Created starter classes and file

---
+++ Easy-Practice/src/main/java/steps_to_zero/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package steps_to_zero;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int numberOfSteps(int num){
+        
+    }
+}
