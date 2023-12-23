4920c94fa97cf0058c300ff023ea3c285ff55106
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-23 13:20:02 -0500

    Added code in controller to initialize matrix & display results

--- Easy-Practice/src/main/java/richest_customer_wealth/Controller.java
+++ Easy-Practice/src/main/java/richest_customer_wealth/Controller.java
@@ -10,6 +10,10 @@ package richest_customer_wealth;
  */
 public class Controller {
     public static void main(String[] args) {
+        Solution s = new Solution();
         
+        int[][] accounts = {{2,8,7}, {7,1,3}, {1,9,5}};
+        
+        System.out.println(s.maximumWealth(accounts));
     }
 }
