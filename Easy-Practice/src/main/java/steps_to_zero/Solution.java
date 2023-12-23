78029b7b49821f906aa563c376b5392f6f363b5a
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-23 13:03:27 -0500

    Added code to solve the problem.

--- Easy-Practice/src/main/java/steps_to_zero/Solution.java
+++ Easy-Practice/src/main/java/steps_to_zero/Solution.java
@@ -10,6 +10,16 @@ package steps_to_zero;
  */
 public class Solution {
     public int numberOfSteps(int num){
+        int steps = 0;
         
+        while(num > 0){
+            if (num % 2 == 0) { num /= 2; } 
+            else { num--; }
+            
+            steps++;
+        }
+
+        
+        return steps;
     }
 }
