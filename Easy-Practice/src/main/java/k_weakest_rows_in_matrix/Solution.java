daa70852bd349cc1a97304777216a3c6593d452c
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2023-12-22 23:52:22 -0500

    Added some psuedocode and notes to break down initial thought process. Created variables and loop.

--- Easy-Practice/src/main/java/k_weakest_rows_in_matrix/Solution.java
+++ Easy-Practice/src/main/java/k_weakest_rows_in_matrix/Solution.java
@@ -10,6 +10,18 @@ package k_weakest_rows_in_matrix;
  */
 public class Solution {
     public int[] kWeakeastRows(int[][] mat, int k){
+        //Weakest possible row is all 0s
+        //Start at the end of each 
+        //Variable to hold next rank 
+        //Array to hold ranks
+        //int[row][column]
         
+        int[] ranks = new int[mat.length];
+        int nextRank;
+        
+        //i goes down the rows, j goes backwards across the columns
+        for(int i = 0, j = mat[0].length-1; i < mat.length; i++, j--){
+            
+        }
     }
 }
