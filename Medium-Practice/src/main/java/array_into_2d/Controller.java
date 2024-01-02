34617bbf1b5056c735a712c18ed755ec36e975ed
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-02 18:08:19 -0500

    Created Medium-Practice Project. Created classes for new problem: Convert an Array Into a 2D Array With Conditions.

---
+++ Medium-Practice/src/main/java/array_into_2d/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package array_into_2d;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        
+        
+    }
+}
---
+++ Medium-Practice/src/main/java/array_into_2d/Solution.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package array_into_2d;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public List<List<Integer>> findMatrix(int[] nums){
+        
+    }
+}
