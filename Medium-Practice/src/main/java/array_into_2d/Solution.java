5ae745c95a45ca0c029c25fc8f0651018b3c57ce
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-02 18:37:12 -0500

    Created instruction file. Started code for quicksort for the first step of the solution.

--- Medium-Practice/src/main/java/array_into_2d/Solution.java
+++ Medium-Practice/src/main/java/array_into_2d/Solution.java
@@ -4,6 +4,7 @@
  */
 package array_into_2d;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,6 +13,24 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> findMatrix(int[] nums){
+        List<List<Integer>> matrix = new ArrayList<List<Integer>>();
+        
+        
+    }
+    
+    public static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    
+    public static void swap(int[] arr, int i, int j){
+    
+    }
+    
+    public static void partition(int[]arr, int low, int high){
         
     }
 }
