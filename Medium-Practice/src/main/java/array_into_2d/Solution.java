f0ec114cf701816d7d47c235b236b18b87823fb6
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-03 20:19:36 -0500

    Added code for swap and partition methods used for quicksort.

--- Medium-Practice/src/main/java/array_into_2d/Solution.java
+++ Medium-Practice/src/main/java/array_into_2d/Solution.java
@@ -27,10 +27,23 @@ public class Solution {
     }
     
     public static void swap(int[] arr, int i, int j){
-    
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
-    public static void partition(int[]arr, int low, int high){
+    public static int partition(int[]arr, int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
         
+        for(int j = low; j < high - 1; j++){
+            if(arr[j] <= pivot){
+                i++;
+                swap(arr, i , j);
+            }
+        }
+        
+        swap(arr, i+1, high);
+        return i+1;
     }
 }
