5ac0b74a7e7ec7001f17c8980f55a73c45dcf5ec
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-09 00:15:46 -0500

    added code to create answer list using a string of the sum.

--- Medium-Practice/src/main/java/add_two_numbers/Solution.java
+++ Medium-Practice/src/main/java/add_two_numbers/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 //        Step 1: Create two variables:
 //            a) an int to calculate sum: start with 0
 //            b) an int to determine multiplying factor: start with 1
-        int sum = 0, mFactor = 1;
+        long sum = 0, mFactor = 1;
         boolean secondList = false;
 //        Step 3: While node is not null:
         while(l1 != null){
@@ -34,30 +34,11 @@ public class Solution {
 //        Step 5) Create variables: 
 //            a) a ListNode that will end up with the head of the answer list
         ListNode prevNode = null, nextNode = null;
-/*        
-//        Step 6) Calculate what factor of 10 the sum is
-        mFactor = (int) Math.pow(10, (String.valueOf(sum).length()-1));
-//        
-//        Step 7) While sum is greater than zero:
-        while(sum > 0){
-//            a) Create next node (we are starting at the end and moving towards the head)
-            ListNode nextNode = new ListNode();
-//            b) set nextNode's val to sum divided by the current factor of 10;
-            nextNode.val = sum/mFactor;
-//            c) set nextNode's next to prevNode
-            nextNode.next = prevNode;
-//            d) set sum as sum minus sum's largest digit
-            sum -= nextNode.val * mFactor;
-//            e) set factor to factor divided by 10
-            mFactor /= 10;
-//            f) set prevNode to nextNode
-            prevNode = nextNode;
-        }
-*/
+        
         String sumString = String.valueOf(sum);
-        while(sumString != ""){
-            nextNode = new ListNode(Integer.valueOf(sumString.charAt(0)), prevNode);
-            sumString = sumString.substring(1);
+        for(int i = 0; i < sumString.length(); i++){
+            nextNode = new ListNode(Character.getNumericValue(sumString.charAt(i)), prevNode);
+            prevNode = nextNode;
         }
         return nextNode;
     }
