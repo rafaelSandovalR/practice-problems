1a33d7bb1cf4da63c0cc51248b8875fdbebd4a5f
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-08 22:41:52 -0500

    Added more pseudocode for creating answer linked list

--- Medium-Practice/src/main/java/add_two_numbers/Solution.java
+++ Medium-Practice/src/main/java/add_two_numbers/Solution.java
@@ -27,16 +27,20 @@ public class Solution {
 //        Step 4) Repeat Step 2 & 3 for List 2;
 //        
 //        Step 5) Create variables: 
-//            a) a ListNode for the head of the answer list
-        ListNode ans = new ListNode();
-        ListNode nextNode = ans;
+//            a) a ListNode that will end up with the head of the answer list
+        ListNode prevNode = null;
 //            b) an int that holds what factor of ten the sum is:
 //        
 //        Step 6) Calculate what factor of 10 the sum is
 //        
 //        Step 7) While sum is greater than zero:
-//            a) set next node's val to sum divided by the current factor of 10;
-//            b) 
-        return ans;
+//            a) Create next node (we are starting at the end and moving towards the head)
+        ListNode nextNode = new ListNode();
+//            b) set nextNode's val to sum divided by the current factor of 10;
+//            c) set nextNode's next to prevNode
+//            d) set sum as sum minus sum's largest digit
+//            e) set factor to factor divided by 10
+//            f) set prevNode to nextNode
+        return nextNode;
     }
 }
