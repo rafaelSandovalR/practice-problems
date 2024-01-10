fc0280d8043358a7755c4adbce06d4555be36c36
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-09 19:13:23 -0500

    Added comment algorithm for new solution

--- Medium-Practice/src/main/java/add_two_numbers/Solution.java
+++ Medium-Practice/src/main/java/add_two_numbers/Solution.java
@@ -15,55 +15,25 @@ import java.util.List;
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2){
         
-        ArrayList<Integer> list1 = new ArrayList<>(), list2 = new ArrayList<>();
-        ListNode temp = l1;
-        boolean secondList = false;
+        /*
         
-        while(temp != null){
-            if(secondList){
-                list2.add(temp.val);
-            } else{
-                list1.add(temp.val);
-            }
-            temp = temp.next;
-            if(temp == null & !secondList){
-                temp = l2;
-                secondList = true;
-            }
-        }
-        
-        int l1Size = list1.size(), l2Size = list2.size();
-        int[] sum = new int[Math.max(l1Size, l2Size) + 1];
-        int startingIndex = sum.length-1;
-        
-        for(int p1 = 0, p2 = 0, p3 = 0, subSum = 0; p1 < l1Size || p2 < l2Size; p1++, p2++, p3++){
-            if(p1 < l1Size && p2 < l2Size){
-                subSum = list1.get(p1) + list2.get(p2);
-            }
-            if(p1 >= l1Size){
-                subSum = list2.get(p2);
-            }
-            if(p2 >= l2Size){
-                subSum = list1.get(p1);
-            }
+        1. Create variable carry to hold carry value after each addition
+        2. Create first answer node, prev node, next node, int var for sums
+        3. While first node and second node are BOTH not null
+            a. Create next node
+            b. Set prev node's next to next node
+            c. If neither nodes are null:
+                i.      add both node vals and carry to sum
+                ii.     set carry by dividing sum by 10
+                iii.    set next node val by taking mod 10 of sum
+            d. If first node is null, add second node val to next node
+            e. If second node is null, add first node val to next node
+            f. set nodes to their respective next nodes
+        4. If carry > 0, create new next node with value.
+        5. return first answer node
             
-            sum[p3] = sum[p3] + subSum % 10;
-            sum[p3+1] = sum[p3] / 10;
-            sum[p3] = sum[p3] % 10;
-            sum[p3+1] = sum[p3+1] + subSum / 10;
-            
-            //node count to avoid leading zeros
-            if(p3+1 == sum.length-1 && sum[p3+1] == 0){
-                startingIndex = p3;
-            }
-        }
         
-        ListNode prevNode = null, nextNode = null;
-
-        for(int i = startingIndex; i >= 0; i--){
-            nextNode = new ListNode(sum[i], prevNode);
-            prevNode = nextNode;
-        }
+        */
         
         return nextNode;
     }
