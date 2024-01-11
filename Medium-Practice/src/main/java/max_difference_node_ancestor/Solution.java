8588569543959f33ddeff8e36fdbe9c188a96092
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-11 16:45:21 -0500

    Added pseudocode

--- Medium-Practice/src/main/java/max_difference_node_ancestor/Solution.java
+++ Medium-Practice/src/main/java/max_difference_node_ancestor/Solution.java
@@ -10,11 +10,23 @@ package max_difference_node_ancestor;
  */
 public class Solution {
     public int maxAncestorDiff(TreeNode root){
-        int maxVal;
+        int maxVal = 0;
         TreeNode a, b;
         
         //Idea: Recursive function
+        //Go Farthest Left and Go Farther Right
         
+        /*
         
+        1. Set a to root;
+        2. While a's left node is not null
+            a. Set b to a's left node
+        3. Set maxVal to a-b
+        4. While a's right node is not null
+            a. Set b to a's right node
+        3. If a-b is more than maxVal, set maxVal to it.
+        
+        */
+        return maxVal;
     }
 }
