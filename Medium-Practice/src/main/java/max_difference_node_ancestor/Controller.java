c1aee88a6c52d45bf26efe8f058d0c9ac7ccb7dd
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-10 23:38:09 -0500

    NEW PROBLEM: Maximum Difference between Node & Ancestor Created starter classes and file

---
+++ Medium-Practice/src/main/java/max_difference_node_ancestor/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package max_difference_node_ancestor;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    Solution s = new Solution();
+}
---
+++ Medium-Practice/src/main/java/max_difference_node_ancestor/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package max_difference_node_ancestor;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int maxAncestorDiff(TreeNode root){
+        
+    }
+}
