37f9ef349daa4fc95416941f9fdb8104ea2e35e5
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-15 18:23:01 -0500

    Deleted previous code for entirely new solution. Created secondary method for recursion that passes high and min of prev nodes.

--- Medium-Practice/src/main/java/max_difference_node_ancestor/Solution.java
+++ Medium-Practice/src/main/java/max_difference_node_ancestor/Solution.java
@@ -9,40 +9,13 @@ package max_difference_node_ancestor;
  * @author Rsand
  */
 public class Solution {
+        
     public int maxAncestorDiff(TreeNode root){
-        int maxVal = 0;
-        int leftMax = 0;
-        int leftMin = 0;
-        int rightMax = 0; 
-        int rightMin = 0;
-        
-        if(root == null){ return 0; }
-        if(root.left == null){ leftMax = 0; leftMin = 0;}
-        else{ 
-            leftMin = Math.min(root.val, root.left.val);
-            leftMax = Math.max(root.val, root.left.val);
-        }
-        if(root.right == null){ rightMax = 0; rightMin = 0;}
-        else{
-            rightMin = Math.min(root.val, root.right.val);
-            rightMax = Math.min(root.val, root.right.val);
-        }
-
-        maxVal = Math.max(leftMax-leftMin, rightMax=rightMin);
-        
-        /*
-        THOUGHTS
-        
-        - Need to make two recursive calls with left node and right node.
-        = Need to return max difference 
-        
-           1
-         2   3
-        4 5 6 7
-        
-        
-        */
-        
-        return maxVal;
+        return 0;
     }
+    
+    public int findMaxDiff(TreeNode ancestor, int prevMax, int prevMin){
+        return 0;
+    }
+    
 }
