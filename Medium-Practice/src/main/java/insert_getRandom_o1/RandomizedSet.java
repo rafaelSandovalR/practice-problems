bd4bfe2ab6822ef980fb40191ee702038abd6166
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-16 15:37:49 -0500

    added hashmap and code for inserting and removing. Ran into issue getting random val

--- Medium-Practice/src/main/java/insert_getRandom_o1/RandomizedSet.java
+++ Medium-Practice/src/main/java/insert_getRandom_o1/RandomizedSet.java
@@ -4,25 +4,40 @@
  */
 package insert_getRandom_o1;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Rsand
  */
 public class RandomizedSet {
     
+    HashMap<Integer, Integer> set = new HashMap<Integer, Integer>();
+    
     public RandomizedSet(){
+
     
     }
     
     public boolean insert(int val){
+        if(set.get(val) == null){ 
+            set.put(val, val);
+            return true;
+        }
         
+        return false;
     }
     
     public boolean remove(int val){
+        if(set.get(val) != null){
+            set.remove(val);
+            return true;
+        }
         
+        return false;
     }
     
     public  int getRandom(){
-        
+
     }
 }
