cff0da594b6b8eba15af7fc39a3d9c9929ea3cef
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-17 16:28:53 -0500

    Replaced code with final solution using HashMap, Array, and Index tracker

--- Medium-Practice/src/main/java/insert_getRandom_o1/RandomizedSet.java
+++ Medium-Practice/src/main/java/insert_getRandom_o1/RandomizedSet.java
@@ -4,10 +4,10 @@
  */
 package insert_getRandom_o1;
 
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 
 /**
@@ -15,30 +15,56 @@ import java.util.Set;
  * @author Rsand
  */
 public class RandomizedSet {
-    Set<Integer> s;
-    Random random;
+    private final Random random;
+    private final Map<Integer, Integer> map;
+    private int[] vals;
+    private int insertIndex;
     
     public RandomizedSet(){
-        s = new HashSet<>();
         random = new Random();
+        map = new HashMap<>();
+        vals = new int[32];
+        insertIndex = 0;
     }
     
     public boolean insert(int val){
-        return s.add(val);
+        if(map.containsKey(val)){
+            return false;
+        }
+        
+        map.put(val, insertIndex);
+        
+        //creates a copy of the array with double the size if necessary
+        if(insertIndex >= vals.length){
+            vals = Arrays.copyOf(vals,vals.length * 2);
+        }
+        
+        vals[insertIndex++] = val;
+        return true;
     }
     
     public boolean remove(int val){
-        return s.remove(val);
+        //saves array index for removed item
+        Integer removed = map.remove(val);
+        if(removed == null) return false;
+        
+        /*  if the item removed was not the last item in the array
+            then it places the last item in the array into the index
+            of the removed item. Then updates hashmap key/value pair
+            to reflect changes
+        */
+        if(removed < insertIndex - 1){
+            vals[removed] = vals[insertIndex-1];
+            map.put(vals[insertIndex-1],removed);
+        }
+        
+        //insertIndex decrements to overwrite item in the last index
+        insertIndex--;
+        return true;
     }
     
     public  int getRandom(){
-        int randomIndex = random.nextInt(s.size());
-        Iterator<Integer> iterator = s.iterator();
-        
-        for(int i = 0; i < randomIndex; i++){
-            iterator.next();
-        }
-        
-        return iterator.next();
+        int index = random.nextInt(insertIndex);
+        return vals[index];
     }
 }
