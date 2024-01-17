5f4c455ffcaf2d4043b9a589c62cb2b00d491136
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-17 15:29:43 -0500

    Added new solution with HashSet and new process of getting random

--- Medium-Practice/src/main/java/insert_getRandom_o1/RandomizedSet.java
+++ Medium-Practice/src/main/java/insert_getRandom_o1/RandomizedSet.java
@@ -4,27 +4,41 @@
  */
 package insert_getRandom_o1;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 
 /**
  *
  * @author Rsand
  */
 public class RandomizedSet {
-    
+    Set<Integer> s;
+    Random random;
     
     public RandomizedSet(){
-
+        s = new HashSet<>();
+        random = new Random();
     }
     
     public boolean insert(int val){
-
+        return s.add(val);
     }
     
     public boolean remove(int val){
-
+        return s.remove(val);
     }
     
     public  int getRandom(){
-
+        int randomIndex = random.nextInt(s.size());
+        Iterator<Integer> iterator = s.iterator();
+        
+        for(int i = 0; i < randomIndex; i++){
+            iterator.next();
+        }
+        
+        return iterator.next();
     }
 }
