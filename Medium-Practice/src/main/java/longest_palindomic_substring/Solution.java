d7d31dcc3c8d63f5ce06ffd2dc86e3f53b752e9e
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-22 17:40:58 -0500

    Deleted code to try new and simpler process

--- Medium-Practice/src/main/java/longest_palindomic_substring/Solution.java
+++ Medium-Practice/src/main/java/longest_palindomic_substring/Solution.java
@@ -10,58 +10,45 @@ package longest_palindomic_substring;
  */
 public class Solution {
     public String longestPalindrome(String s){
+
         /*
-            1. Work on string itself
-            2. Set index var to size of string - 1
-            3. Create vars for:
-                a. prev
-                b. preprev
-                c. maxSize
-                d. currentSize
-                e. Starting Index
-                b. Ending Index
-            4. For the size of the string
-                b. if currentChar is equals to prevChar AND prevIndex > 0
-                    i. currentSize = 2
-                    ii. while nextChar equals prevChar--
-                        1. currentSize++
-                        2. startingIndex = prev
-                        3. endingIndex = next
-                c. if currentChar is equals to prePrevChar AND prevIndex > 0
-                    i. currentSize = 3
-                    ii. while nextChar equals 4xprev char--
-                        1. currentSize++
-                        2. startingIndex = prev
-                        3. endingIndex = next
-                d. Set max to max(max, currentsize)
-            5. Return max
+            1. Compare current character to previous character
+            2. If characters match:
+                a. Update current size
+                b. Check surrounding characters.
         */
+
         
-        int size = s.length();
-        int currentSize = 1, maxSize = 0;
-        int startIndex, endIndex;
+    
+        int startIndex = 0, endIndex = 0;
+        int currentSize = 0, maxSize = 0;
+
         
-        for (int i = 1, prev = 0; i < size; i++){
-            if (prev > 0 && s.charAt(i) == s.charAt(prev)){
-                currentSize++;
-                startIndex = prev;
-                while (i < size && s.charAt(++i) == s.charAt(--prev)){
-                    currentSize++;
-                    endIndex = i; 
+        for (int forward = 1, reverse = 0; forward < s.length(); forward++){
+            
+            if(s.charAt(forward) == s.charAt(reverse)){
+                //update size
+                currentSize+=2;
+                
+                //update max and indices
+                if (currentSize > maxSize) {
+                    maxSize = currentSize;
+                    startIndex = reverse;
+                    endIndex = forward;
                 }
+                
+                reverse--;
             }
-            if (prev > 0 && s.charAt(i) == s.charAt(prev-1)){
-                currentSize++;
-                startIndex = prev;
-                while (i < size && s.charAt(++i) == s.charAt(--prev)){
-                    currentSize++;
-                    endIndex = i;
-                }
+            else{
+                reverse = forward;
             }
             
-            maxSize = Math.max(maxSize, currentSize);
+
+            
         }
         
+        
+        return s.substring(startIndex, endIndex);
     
     }
 }
