67d7003b925f61f4673b80e21a7d9b2f72a542ad
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-29 19:58:35 -0500

    NEW PROBLEM: Zigzag Conversion Created starter classes and file

--- Medium-Practice/src/main/java/zigzag_conversion/Controller.java
+++ Medium-Practice/src/main/java/zigzag_conversion/Controller.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package longest_palindomic_substring;
+package zigzag_conversion;
 
 /**
  *
@@ -10,11 +10,6 @@ package longest_palindomic_substring;
  */
 public class Controller {
     public static void main(String[] args) {
-        Solution s = new Solution();
         
-        String test = "axblovolllovolbxa";
-        String test2 = "aaaa";
-        
-        s.longestPalindrome(test2);
     }
 }
---
+++ Medium-Practice/src/main/java/zigzag_conversion/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zigzag_conversion;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public String convert(String s, int numRows){
+        
+    }
+}
