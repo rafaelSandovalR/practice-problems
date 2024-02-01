fc9b62faa72e62e8267a240227b62e8eb65ed806
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-01-31 21:36:40 -0500

    NEW PROBLEM: Reverse Integer Created started classes and file

---
+++ Medium-Practice/src/main/java/reverse_integer/Solution.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reverse_integer;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int reverse(int x){
+        
+        /*
+            1. Create a stack to place each digit in lifo order and a stack for reverse
+            2. Push each digit starting from the left, to the first stack
+            3. Pop each digit and push it to the reverse stack
+            4. 
+        */
+    }
+}
