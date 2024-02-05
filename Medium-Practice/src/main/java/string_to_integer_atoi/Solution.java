8cfa3ded4a02965f5d774cd55fffdd8449476605
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-05 13:01:38 -0500

    Added pseudocode

--- Medium-Practice/src/main/java/string_to_integer_atoi/Solution.java
+++ Medium-Practice/src/main/java/string_to_integer_atoi/Solution.java
@@ -11,7 +11,15 @@ package string_to_integer_atoi;
 public class Solution {
     public int myAtoi(String s){
         /*
-            
+            1. Read in and ignore any leading whitespace
+            2. Check if the next character is '-' or '+' (if not already at the end of the string)
+            3. Read in the next characters until the next non-digit character or end of input.
+            4. Convert digits into an integer.
+            5. If no digits are read then it is 0. Ignore non-digit characters.
+            6. Integer must be 32 bit. 
+            7. Return final result
         */
+        
+        
     }
 }
