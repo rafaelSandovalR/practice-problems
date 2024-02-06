050d8722cae635cfc32425c864217682d25a672d
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-06 16:40:13 -0500

    NEW PROBLEM: Container with Most Water Created starter classes and file. Added variables and loop to calculate max volume

---
+++ Medium-Practice/src/main/java/container_with_most_water/Solution.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package container_with_most_water;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int maxArea(int[] height){
+   
+        int currentVol = 0, maxVol = 0;
+        
+        for(int i = 0, j = height.length-1; i < j;){
+            
+            currentVol = Math.min(height[i], height[j]) * (j-i);
+            
+            maxVol = Math.max(currentVol, maxVol);
+            
+            if (height[i] < height[j]){ i++; }
+            if (height[i] > height[j]){ j++; }
+            if (height[i] == height[j]){ i++; j++; }
+            
+        }
+    }
+}
