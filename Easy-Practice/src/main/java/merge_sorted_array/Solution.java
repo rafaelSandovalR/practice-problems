f4c5d25fccb5bda938d0721925e3a6ee50ee1cf5
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-07 18:45:53 -0500

    Pseudocode for initial algorithm

--- Easy-Practice/src/main/java/merge_sorted_array/Solution.java
+++ Easy-Practice/src/main/java/merge_sorted_array/Solution.java
@@ -11,5 +11,14 @@ package merge_sorted_array;
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n){
         
+        /*
+            1. Set var ptr1 as length of usable numbers in first list: m-n
+            2. Set var ptr2 as last index of second list
+            3. Set var set as last index in first list
+            4. While set is greater than 0
+                a. if number in ptr1 is larger or equal, put it in the set index
+                b. else put the number in ptr2 in the set index
+                c. decrement set index
+        */
     }
 }
