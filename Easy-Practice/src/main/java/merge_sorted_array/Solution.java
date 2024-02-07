977c515df7056dbf54472929aba64fbfd70bc0f2
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-07 17:15:06 -0500

    NEW EASY PROBLEM: Merge Sorted Arrays Created classes and file

---
+++ Easy-Practice/src/main/java/merge_sorted_array/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package merge_sorted_array;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        
+    }
+}
