a21da88906e58a3837a58b3d8653f7c6796525bd
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-07 20:49:14 -0500

    rewrote solution. New solution is more concise. Uses two pointers that traverse in same direction and overwrites any "val" matches.

--- Easy-Practice/src/main/java/remove_element/Solution.java
+++ Easy-Practice/src/main/java/remove_element/Solution.java
@@ -10,40 +10,16 @@ package remove_element;
  */
 public class Solution {
     public int removeElement(int[] nums, int val){
-        
-        /*
-            1. Create two pointer vars. One to start at each end.
-            2. Create var for count
-            3. If ptr1 number equals val:
-                a. increment count
-                b. If ptr2 number equals val
-                    i.  increment count
-                    ii. decrement ptr2
-                    iii. loop
-                c. Swap ptr1 number with ptr2 number
-                d. loop
-            4. return count
-            
-        */
-        
-        int ptr1 = 0, ptr2 = nums.length-1, count = nums.length;
-        
-        while (ptr1 < ptr2){
-            if (nums[ptr1] == val){
-                count--;
                 
-                while(nums[ptr2] == val){
-                    count--;
-                    ptr2--;
-                }
-                
-                nums[ptr1] = nums[ptr2];
-                ptr2--;
+        int count = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
             }
-            
-            ptr1++;
         }
-        
+
         return count;
     }
 }
