368ffa2a5042b74fe0b1da4235fda89e58f7971f
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-08 15:45:09 -0500

    Commented alt solution without the need for a "prev" variable.

--- Easy-Practice/src/main/java/remove_dups_from_sorted_array/Solution.java
+++ Easy-Practice/src/main/java/remove_dups_from_sorted_array/Solution.java
@@ -19,6 +19,23 @@ public class Solution {
                 count++;
             }
         }
+        
+        /*
+        Alternate Solution with no prev variable
+        ----------------------------------------
+        
+        int count = 0;
+        
+        for(int i = 1; i < nums.length; i++){
+            if (nums[i] != nums[count]){
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+              
+        return count+1;
+        
+        */
               
         return count;
     }
