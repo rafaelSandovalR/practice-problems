b3bb93446b2f58ecd0abe89d179e9c7588113e4a
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-07 19:00:54 -0500

    added variables and loop with ternary operation

--- Easy-Practice/src/main/java/merge_sorted_array/Solution.java
+++ Easy-Practice/src/main/java/merge_sorted_array/Solution.java
@@ -12,13 +12,19 @@ public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n){
         
         /*
-            1. Set var ptr1 as length of usable numbers in first list: m-n
+            1. Set var ptr1 as length of usable numbers in first list
             2. Set var ptr2 as last index of second list
             3. Set var set as last index in first list
             4. While set is greater than 0
                 a. if number in ptr1 is larger or equal, put it in the set index
                 b. else put the number in ptr2 in the set index
                 c. decrement set index
-        */
+        */  
+        
+        int ptr1 = m-1, ptr2 = n-1;
+        
+        for(int set = m+n-1; set < 0; set--){
+            nums1[set] = nums1[ptr1] >= nums2[ptr2] ? nums1[ptr1--] : nums2[ptr2--];
+        }
     }
 }
