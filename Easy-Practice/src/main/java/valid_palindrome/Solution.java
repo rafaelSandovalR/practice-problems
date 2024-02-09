b77ae37ddc8f22650541704b411a02fff23443e1
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-09 16:50:03 -0500

    removed unnecessary code. added character to lower case methods inside loops to find alphanum chars

--- Easy-Practice/src/main/java/valid_palindrome/Solution.java
+++ Easy-Practice/src/main/java/valid_palindrome/Solution.java
@@ -14,29 +14,22 @@ public class Solution {
         if(s.length() == 1) return true;
 
         char char1, char2;
-        int charCount = 0;
         int i = 0, j = s.length()-1;
-        boolean atLeastOneCharacter = false;
         
         while(i<j){
             
             char1 = Character.toLowerCase(s.charAt(i));
             char2 = Character.toLowerCase(s.charAt(j));
             
-            while (!isAlphanumeric(char1) && i < j - 1) {
-                char1 = s.charAt(++i);
+            while (!isAlphanumeric(char1) && i<j) {
+                char1 = Character.toLowerCase(s.charAt(++i));
             }
-            while (!isAlphanumeric(char2) && j > i + 1) {
-                char2 = s.charAt(--j);
+            while (!isAlphanumeric(char2) && j>i) {
+                char2 = Character.toLowerCase(s.charAt(--j));
             }
             
-            if(!atLeastOneCharacter && (isAlphanumeric(char1) || isAlphanumeric(char2))){
-                atLeastOneCharacter = true;
-            }
 
             if (isAlphanumeric(char1) && isAlphanumeric(char2)) {
-                charCount++;
-
                 if (char1 != char2) {
                     return false;
                 }
@@ -46,7 +39,7 @@ public class Solution {
             j--;
         }
         
-        return (charCount > 0 || atLeastOneCharacter);
+        return true;
     }
     
     public boolean isAlphanumeric(char c){
