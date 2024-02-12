5bf4938aaf8aabb1958b400f820d52c03bbf7f1b
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-12 12:42:09 -0500

    Added test cases in Controller class. Added initial solution.

--- Easy-Practice/src/main/java/is_subsequence/Controller.java
+++ Easy-Practice/src/main/java/is_subsequence/Controller.java
@@ -10,6 +10,12 @@ package is_subsequence;
  */
 public class Controller {
     public static void main(String[] args) {
-        Solution s = new Solution();
+        Solution sol = new Solution();
+        
+        String s = "abc";
+        String t = "ahbgdc";
+        
+        boolean ans = sol.isSubsequence(s, t);
+        
     }
 }
--- Easy-Practice/src/main/java/is_subsequence/Solution.java
+++ Easy-Practice/src/main/java/is_subsequence/Solution.java
@@ -11,5 +11,15 @@ package is_subsequence;
 public class Solution {
     public boolean isSubsequence(String s, String t){
     
+        for(int i=0, j=0; i < t.length(); i++){
+            
+            if(t.charAt(i) == s.charAt(j)){
+                j++;
+                
+                if(j == s.length()){return true;}
+            }
+        }
+        
+        return false;
     }
 }
