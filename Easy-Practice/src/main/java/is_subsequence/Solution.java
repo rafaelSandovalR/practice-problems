3576bd89609550faad4b52326d54e501d05bc222
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-12 13:02:09 -0500

    Added alternate solution for quicker runtime

--- Easy-Practice/src/main/java/is_subsequence/Solution.java
+++ Easy-Practice/src/main/java/is_subsequence/Solution.java
@@ -10,18 +10,37 @@ package is_subsequence;
  */
 public class Solution {
     public boolean isSubsequence(String s, String t){
-        
-        if(s.length() == 0){return true;}
+        /*
+                if(s.length() == 0){return true;}
     
-        for(int i=0, j=0; i < t.length(); i++){
-            
-            if(t.charAt(i) == s.charAt(j)){
-                j++;
-                
-                if(j == s.length()){return true;}
-            }
-        }
+                for(int i=0, j=0; i < t.length(); i++){
+
+                    if(t.charAt(i) == s.charAt(j)){
+                        j++;
+
+                        if(j == s.length()){return true;}
+                    }
+                }
         
+                return false;
+        */
+        
+        int l = 0, r = 0;
+        
+        char[] main = t.toCharArray();
+        char[] sub = s.toCharArray();
+        
+        if (sub.length == 0){ return true; }        
+        if (main.length < sub.length){ return false; }
+        
+        while (l < main.length){
+            char curr = main[1];
+            if(curr == sub[r]){
+                r++;
+                if(r == sub.length){ return true; }
+            }
+            l++;
+        }
         return false;
     }
 }
