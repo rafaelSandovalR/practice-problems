6321911c6bd854b84ab9e0a51d049c475395226c
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-12 12:45:52 -0500

    Added base case for empty String s

--- Easy-Practice/src/main/java/is_subsequence/Solution.java
+++ Easy-Practice/src/main/java/is_subsequence/Solution.java
@@ -10,6 +10,8 @@ package is_subsequence;
  */
 public class Solution {
     public boolean isSubsequence(String s, String t){
+        
+        if(s.length() == 0){return true;}
     
         for(int i=0, j=0; i < t.length(); i++){
             
