6531f6e23e80f4d6794ec284997e6b2bddf419a6
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-14 14:10:41 -0500

    Minimum Size Array Subarray Sum:
    Added pseudocode for first solution idea

--- Medium-Practice/src/main/java/minimum_size_subarray_sum/Solution.java
+++ Medium-Practice/src/main/java/minimum_size_subarray_sum/Solution.java
@@ -10,6 +10,31 @@ package minimum_size_subarray_sum;
  */
 public class Solution {
     public int minSubArrayLen(int target, int[] nums){
-        
+        /*
+            1. Use two pointers: start and end
+                a. start is the point where to start calculation
+                b. end is the end of the range for the calculation
+            2. Create vars: 
+                a. to hold minimum sub array length
+                b. to hold current sub array length
+                c. current sum in each inner loop
+            3. While START is less than nums length
+                a. Check if START is equal to or greater than target
+                    i. if so: set MIN to 1
+                b. Increment END
+                c. If END is equal to nums length
+                    i. Break
+                b. Else
+                    i. Start loop to get subarray while END is less than nums length 
+                        OR current sum is less than target
+                        1. Set CURRENT SUM to Start + End
+                        2. If CURRENT SUM is greater than or equal to target
+                            a. Set CURRENT SUM equal to 0
+                            b. set MIN length to MIN(MIN, END minus start)
+                        3. Else
+                            a. Increment END
+                    ii. Set START to equal END 
+            4. Return MIN
+        */
     }
 }
