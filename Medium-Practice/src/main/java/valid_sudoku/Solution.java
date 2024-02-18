eb2a2203ecb77dcff78d24e3599bb8b6f6d360b2
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-17 23:23:48 -0500

    Valid Sudoku Added pseudocode for initial idea

--- Medium-Practice/src/main/java/valid_sudoku/Solution.java
+++ Medium-Practice/src/main/java/valid_sudoku/Solution.java
@@ -10,6 +10,15 @@ package valid_sudoku;
  */
 public class Solution {
     public boolean isValidSudoku(char[][] board){
+        /*
+            1. Make boolean array 0-9, to determine if duplicates exist within
+                3x3 grid
+            2. Compare first value with items in first row
+                - save first three items 
+            3. Compare with items in first column.
+            4. 
+            5. 
+        */
         
     }
 }
