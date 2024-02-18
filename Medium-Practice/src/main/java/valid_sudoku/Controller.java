f23d514c609ca1e7ad06e52f0231acb3c4bc251e
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-17 22:52:01 -0500

    NEW PROBLEM: Valid Sudoku Created classes and file

--- Medium-Practice/src/main/java/valid_sudoku/Controller.java
+++ Medium-Practice/src/main/java/valid_sudoku/Controller.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package minimum_size_subarray_sum;
+package valid_sudoku;
 
 /**
  *
@@ -10,13 +10,6 @@ package minimum_size_subarray_sum;
  */
 public class Controller {
     public static void main(String[] args) {
-        Solution s = new Solution();
-
-        int[] nums = {10,5,13,4,8,4,5,11,14,9,16,10,20,8};
-        int target = 80;
-
-        int minLength = s.minSubArrayLen(target, nums);
         
     }
-
 }
--- Medium-Practice/src/main/java/valid_sudoku/Solution.java
+++ Medium-Practice/src/main/java/valid_sudoku/Solution.java
@@ -2,17 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package longest_substr_no_repeat;
+package valid_sudoku;
 
 /**
  *
  * @author Rsand
  */
-public class Controller {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-         int max = s.lengthOfLongestSubstring("abcabcbb");
+public class Solution {
+    public boolean isValidSudoku(char[][] board){
         
-        System.out.println(max);
     }
 }
