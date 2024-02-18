73adeb08707e47c412ae9db1f831386b2865f634
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-18 16:04:32 -0500

    Valid Sudoku Added more pseudocode for first idea

--- Medium-Practice/src/main/java/valid_sudoku/Solution.java
+++ Medium-Practice/src/main/java/valid_sudoku/Solution.java
@@ -13,11 +13,26 @@ public class Solution {
         /*
             1. Make boolean array 0-9, to determine if duplicates exist within
                 3x3 grid
-            2. Compare first value with items in first row
-                - save first three items 
-            3. Compare with items in first column.
-            4. 
-            5. 
+            2. Check that no duplicates exist in first row, and that valid digits
+                - save first three items (if any)
+            3. Check that no duplicates exist in first column, and valid
+                - save 2nd and 3rd item (if any)
+            4. Check that no duplicates exist in second row, and valid
+                - save 2nd and 3rd item (if any)
+            5. Do same step as step 4 for second column, third row, third column
+            6. For each loop return false if duplicates exist in column or duplicates
+                exist in saved items.
+            7. clear saved items.
+            8. Check for dups and valid for fourth, fifth and sixth columns
+                - save items
+            9. For each loop return false if duplicates exist
+            10. Repeat steps 8 & 9 for sixth, seventh and eight column
+            11. Check for dups in rows 3, 4, 5
+            12. Check for dups in middle quadrant
+            13. check for dups in middle, right quadrant
+            14. check for dups in bottom left quadrant,
+            15. check for dups in bottom middle quadant
+            16. check for dups in bottom right quadrant.
         */
         
     }
