a74ec9718413e1f57bd65afea1ecd0aec79eb952
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-19 12:48:07 -0500

    Valid Sudoku Created three methods for each check: rows, columns, subboxes

--- Medium-Practice/src/main/java/valid_sudoku/Solution.java
+++ Medium-Practice/src/main/java/valid_sudoku/Solution.java
@@ -35,28 +35,82 @@ public class Solution {
             16. check for dups in bottom right quadrant.
         */
         
+        return areRowsValid(board) && areColumnsValid(board) && areSubBoxesValid(board);
+
+    }
+    
+    private boolean areRowsValid(char[][] board){
         int row = 0, column = 0;
-        
-        while (row < 9){
-            boolean[] rowOrColumn = new boolean[9];
-            
+
+        while (row < 9) {
+            boolean[] currentRow = new boolean[10];
+
             while (column < 9) {
                 int charValue = Character.getNumericValue(board[row][column]);
+                column++;
                 if (charValue < 0) {
-                    column++;
                     continue;
                 }
-                if (rowOrColumn[charValue] == true) {
+                if (currentRow[charValue] == true) {
                     return false;
                 }
-                rowOrColumn[charValue] = true;
-
+                currentRow[charValue] = true;
             }
 
             column = 0;
-            row++;        
-      
+            row++;
+
         }
         return true;
+    }
+    
+    private boolean areColumnsValid(char[][] board){
+        int row = 0, column = 0;
+
+        while (column < 9) {
+            boolean[] currentColumn = new boolean[10];
+
+            while (row < 9) {
+                int charValue = Character.getNumericValue(board[row][column]);
+                row++;
+                if (charValue < 0) {
+                    continue;
+                }
+                if (currentColumn[charValue] == true) {
+                    return false;
+                }
+                currentColumn[charValue] = true;
+            }
+
+            row = 0;
+            column++;
+
+        }
+        return true;
+    }
+    
+    private boolean areSubBoxesValid(char[][] board){
+        
+        int row = 0, column = 0, min = 0, max = 2;
+        
+        for(int subBox = 0; subBox < 9; subBox++){
+        
+            boolean[] subBoxCheck = new boolean[10];
+            
+            while (row >= min && row <= max){
+                
+                while (column >= min && column <= max){
+                    int currentValue = Character.getNumericValue(board[row][column]);
+                    
+                    if (subBoxCheck[currentValue] == true) return false;
+                    column++;
+                }
+            }
+            
+        }
+        
+        return true;
+    }
+        
     }
 }
