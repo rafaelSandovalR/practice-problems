06c7f1e194d5e6d1aaaa131c4425049c619a0c51
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-19 15:47:09 -0500

    NEW PROBLEM: SPIRAL MATRIX Created classes and file

---
+++ Medium-Practice/src/main/java/spiral_matrix/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spiral_matrix;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        
+        
+    }
+}
