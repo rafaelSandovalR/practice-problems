e0d2770cc282516d0fde55a2018ae846ece3c4e2
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-19 17:06:50 -0500

    NEW PROBLEM: SPIRAL MATRIX added first attempt at solution

--- Medium-Practice/src/main/java/spiral_matrix/Solution.java
+++ Medium-Practice/src/main/java/spiral_matrix/Solution.java
@@ -4,6 +4,7 @@
  */
 package spiral_matrix;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,5 +14,42 @@ import java.util.List;
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix){
         
+        List<Integer> spiral = new ArrayList<>();
+        int rowMin = 0, rowMax = matrix.length;
+        int colMin = 0, colMax = matrix[0].length;
+        int direction = 1;
+        boolean traversingRow = true;
+        
+        for(int r = 0, c = 0, steps = 0; steps < matrix.length * matrix[0].length; steps++){
+            
+            spiral.add(matrix[r][c]);
+            
+            if (traversingRow){
+                c += direction;
+                
+                if (c == colMax) {
+                    colMax--;
+                    traversingRow = !traversingRow;
+                    r += direction;
+                    c -= direction;
+                }
+                
+            } else {
+                r = r + direction;
+                
+                if (r == rowMax) {
+                    rowMax--;
+                    traversingRow = !traversingRow;
+                    direction *= -1;
+                    c += direction;
+                    r += direction;
+                }
+            }
+           
+        }
+        
+        
+        
+        return spiral;
     }
 }
