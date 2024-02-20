190de84f8130f2e2b1f989e9032848da8a929e9a
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-20 14:27:42 -0500

    EASY PROBLEM REVISIT: RANSOM NOTE replaced code with better solution using smaller array to keep indices of last occurrence of a character

--- Easy-Practice/src/main/java/ransom_note/Solution.java
+++ Easy-Practice/src/main/java/ransom_note/Solution.java
@@ -14,18 +14,31 @@ import java.util.Map;
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine){
         
-        int[] magChars = new int[128];
-        
-        for(int i = 0; i < magazine.length(); i++){
-            magChars[magazine.charAt(i)]++;
+        int[] alphabet = new int[26];
+
+        for (char c : ransomNote.toCharArray()) {
+            
+            // c % 26 : assigns a unique index to each of the 26 characters
+            //          from 0 - 25
+            
+            // alphabet[c % 26] : gets the index value saved at that index that
+            //                      represents the current character
+            
+            // magazine.indexOf : will search for the next index of a character
+            //                      starting from the index of the previous
+            //                      occurence
+                        
+            int i = magazine.indexOf(c, alphabet[c % 26]);
+            
+            if (i == -1) {
+                return false;
+            }
+            
+            // increase index of character to one more than this occurence
+            alphabet[c % 26] = i + 1;
         }
         
-        for(int i = 0; i < ransomNote.length(); i++){
-            if (magChars[ransomNote.charAt(i)]-- == 0) return false;
-        }
- 
         return true;
-        
         
  /* HASHMAP SOLUTION
             //hashmap of letter and their count
