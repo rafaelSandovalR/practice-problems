4ce610dcaf307a9bb03bcc967e2d449d2afb2d59
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-27 16:13:17 -0500

    VALID ANAGRAM Added solution with an int array and two loops to check if anagram

--- Easy-Practice/src/main/java/valid_anagram/Solution.java
+++ Easy-Practice/src/main/java/valid_anagram/Solution.java
@@ -10,7 +10,21 @@ package valid_anagram;
  */
 public class Solution {
     public boolean isAnagram(String s, String t){
+        
+        if (s.length() != t.length()) return false;
         int[] freq = new int[26];
         
+        for (int i=0; i < s.length(); i++){
+            int idx = s.charAt(i % 26);
+            freq[idx]++;
+        }
+        
+        for (int i=0; i < t.length(); i++){
+            int idx = s.charAt(i % 26);
+            if (freq[idx] == 0) return false;
+            freq[idx]--;
+        }
+        
+        return true;
     }
 }
