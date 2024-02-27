bb76450ef20233a052c2241ff79e209c18f5c824
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-02-27 17:10:10 -0500

    NEW MEDIUM PROBLEM: GROUP ANAGRAMS Created Classes and file Wrote pseudocode Started solution

---
+++ Medium-Practice/src/main/java/group_anagrams/Solution.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group_anagrams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        /*
+            - Create Output list
+            - Enter first item into first list
+            - While STRS has more items
+                - While List has more lists
+                    - Compare item to first item of first list
+                    - If anagram, add to list and break
+                    - If end of list, create new list, add item to it and break;
+            = Return List
+        */
+      
+        List<List<String>> ans = new ArrayList<List<String>>();
+        List<String> firstList = new ArrayList<String>();
+        List<int[]> freqList = new ArrayList<int[]>();
+        int[] freq = new int[26];
+        
+        for (int i = 0; i < strs[0].length(); i++) {
+            int idx = strs[0].charAt(i) % 26;
+            freq[idx]++;
+        }
+        
+        freqList.add(freq);
+        firstList.add(strs[0]);
+        ans.add(firstList);
+        
+        for (int i=1; i < strs.length; i++){
+            for (int j=0; j < ans.size(); j++){
+                
+            }
+        }
+        
+        return ans;
+    }
+}
