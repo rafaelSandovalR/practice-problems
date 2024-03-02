d9d4a9cb7e5a232f28bdc17d1a427e501c39f99f
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-02 16:31:09 -0500

    GROUP ANAGRAMS: Replaced code with new solution using an anonymous inner class, hashmap, and StringBuilder

--- Medium-Practice/src/main/java/group_anagrams/Solution.java
+++ Medium-Practice/src/main/java/group_anagrams/Solution.java
@@ -4,36 +4,65 @@
  */
 package group_anagrams;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author Rsand
  */
 public class Solution {
+    
+    private List<List<String>> ans;
+    
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        HashMap<String,List<String>> map = new HashMap<>();
-        
-        for (int i = 0; i < strs.length; i++) {
-            // sort the characters in each string
-            char[] ch = strs[i].toCharArray();
-            Arrays.sort(ch);
-            String sortedStr = new String(ch);
-            
-            // if sorted string does not exist as a key, add new list to hashmap
-            if (!map.containsKey(sortedStr)){
-                map.put(sortedStr, new ArrayList<>());
-            }
-            
-            // add str to hashmap using sorted string as key
-            map.get(sortedStr).add(strs[i]);
-        }
-               
-        
-        return new ArrayList<>(map.values());
+       return new AbstractList<List<String>>() {
+           
+           // overrides abstract get
+           public List<String> get(int index){
+               if (ans == null) init();
+               return ans.get(index);
+           }
+           
+           // overrides abstract size
+           public int size() {
+               if (ans == null) init();
+               return ans.size();
+           }
+           
+           // initializes data
+           private void init() {
+               Map<String, List<String>> map = new HashMap<>();
+               for (String s : strs){
+                   
+                   // determine frequency of characters in string
+                   int[] freq = new int[26];
+                   for (char c : s.toCharArray()){
+                       freq[c - 'a']++;
+                   }
+                   
+                   // build a key for the map
+                   StringBuilder sb = new StringBuilder();
+                   for (int i = 0; i < 26; i++){
+                       if (freq[i] != 0){
+                           
+                           // create a string using each character followed by its frequency
+                           sb.append('a' + i);
+                           sb.append(freq[i]);
+                       }
+                   }
+                   String key = sb.toString();
+                   
+                   // add string to hashmap, create new entry if key is absent
+                   map.computeIfAbsent(key, k -> new ArrayList<String>()).add(s);
+                   ans = new ArrayList<>(map.values());
+               }
+           }
+       };
     }
 }
