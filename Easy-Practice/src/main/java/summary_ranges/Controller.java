aa272856233d34830110e79e5472a047868e3d6d
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-05 17:45:43 -0500

    SUMMARY RANGES Added test code in controller

--- Easy-Practice/src/main/java/summary_ranges/Controller.java
+++ Easy-Practice/src/main/java/summary_ranges/Controller.java
@@ -4,12 +4,21 @@
  */
 package summary_ranges;
 
+import java.util.List;
+
 /**
  *
  * @author Rsand
  */
 public class Controller {
     public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        int[] nums1 = {0,1,2,3,4,5};
+        List<String> ans1 = sol.summaryRanges(nums1);
+        
+        int[] nums2 = {0,1,3,4,5,7,8,9,11};
+        List<String> ans2 = sol.summaryRanges(nums2);
         
     }
 }
