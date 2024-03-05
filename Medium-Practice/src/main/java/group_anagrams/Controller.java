878bcec0e89fa06c141489544fb0794d394466ff
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-05 17:07:21 -0500

    GROUP ANAGRAMS: Added test code in controller and runtime test

--- Medium-Practice/src/main/java/group_anagrams/Controller.java
+++ Medium-Practice/src/main/java/group_anagrams/Controller.java
@@ -4,10 +4,41 @@
  */
 package group_anagrams;
 
+import java.util.List;
+
 /**
  *
  * @author Rsand
  */
 public class Controller {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        
+        long startTime = System.nanoTime();
+        
+        List<List<String>> ans = sol.groupAnagrams(strs);
+        
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        
+        System.out.println("Execution time in nanoseconds: " + elapsedTime);
+        
+        
+        /*
+        RUNTIME + MEMORY:
+            2-29-24 :   797000 nanoseconds
+                        546 ms in Leetcode
+        
+            3-2-24  :   277600 nanoseconds  0.568 s
+                        6 ms in Leetcode
+        
+            3-2-24  :   343600 nanoseconds  0.557 s
+                        0 ms in Leetcode    49.08 MB
+        */
+        
+    }
+
     
 }
