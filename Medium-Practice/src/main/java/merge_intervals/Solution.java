d01bcea64cd97969d3a79038403d38cc8ae1aca9
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-07 18:51:31 -0500

    MERGE INTERVALS Added loop to convert arraylist to int 2d array

--- Medium-Practice/src/main/java/merge_intervals/Solution.java
+++ Medium-Practice/src/main/java/merge_intervals/Solution.java
@@ -29,8 +29,15 @@ public class Solution {
             }
             
             list.add(range);
-        } 
+        }
         
-        return null;
+        int[][] ans = new int[list.size()][2];
+        
+        for (int i = 0; i < list.size(); i++){
+            ans[i][0] = list.get(i)[0];
+            ans[i][1] = list.get(i)[1];
+        }
+        
+        return ans;
     }
 }
