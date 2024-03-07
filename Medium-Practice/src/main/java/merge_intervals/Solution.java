d59b87636afdebe090f58545fc03e5952607c543
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-07 16:57:06 -0500

    MERGE INTERVALS changed arraylist to int from Integer. Removed conversion

--- Medium-Practice/src/main/java/merge_intervals/Solution.java
+++ Medium-Practice/src/main/java/merge_intervals/Solution.java
@@ -14,27 +14,19 @@ import java.util.List;
 public class Solution {
     public int[][] merge(int[][] intervals){
         
-        List<Integer[]> list = new ArrayList<Integer[]>();
+        List<int[]> list = new ArrayList<int[]>();
         
         for (int i = 0; i < intervals.length; i++){
             
-            Integer[] range = {i,1};
+            int[] range = {i,1};
             
             while (i+1 < intervals.length && intervals[i][1] >= intervals[i+1][0]){
                 range[0] = ++i;
             }
             
             list.add(range);
-        }
+        } 
         
-        int[][] ans = new int[list.size()][2];
-        int i = 0;
-        
-        for (Integer[] range: list){
-            ans[i][0] = list.get(i)[0];
-            ans[i][1] = list.get(i)[1];
-        }
-        
-        return ans;
+        return null;
     }
 }
