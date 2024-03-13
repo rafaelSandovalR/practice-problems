4de584765b51077412c8ab28368c7bdd7da5dbf8
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-13 14:37:59 -0400

    MERGE INTERVALS Replaced code with more efficient solution. Using int array to track interval start and end points. Using LinkedList for result.

--- Medium-Practice/src/main/java/merge_intervals/Solution.java
+++ Medium-Practice/src/main/java/merge_intervals/Solution.java
@@ -6,6 +6,7 @@ package merge_intervals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,6 +15,45 @@ import java.util.List;
  */
 public class Solution {
     public int[][] merge(int[][] intervals){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;    
+        
+        // Determine the min and max of the first elements in each interval
+        for (int i = 0; i < intervals.length; i++){
+            min = Math.min(min, intervals[i][0]);
+            max = Math.max(max, intervals[i][0]);
+        }
+        
+        // Array to track the max ending pointed enountered for each starting point
+        int[] range = new int[max - min + 1];
+        for (int i = 0; i < intervals.length; i++){
+            // Records the furthest ending point encountered so far
+            range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]);
+        }
+        
+        // Keeps track of current merged interval
+        int start = 0, end = 0;
+        LinkedList<int[]> result = new LinkedList<>();       
+        for (int i = 0; i < range.length; i++){
+            // Indicates that no interval starting at this point
+            if (range[i] == 0) continue;
+            
+            // If true, indicates that there is an overlap
+            if (i <= end){
+                end = Math.max(range[i], end);
+            } else{
+                // Indicates end of previous merged interval
+                result.add(new int[] {start + min, end + min});
+                // Marks the beginning of a new merged interval
+                start = i;
+                end = range[i];
+            }
+        }
+        // Adds the final merged interval
+        result.add(new int[]{start + min, end + min});
+        return result.toArray(new int[result.size()][]);
+
+        /*  FIRST SOLUTION
         
         if (intervals.length <= 1){
             return intervals;
@@ -37,7 +77,7 @@ public class Solution {
             }
         }
         return result.toArray(new int[result.size()][]);
-        
+        */
     }
     
 }
