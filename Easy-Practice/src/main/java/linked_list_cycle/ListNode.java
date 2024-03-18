617108ce886b26d1465c131c4195555f3caf996f
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-17 22:01:53 -0400

    NEW EASY PROBLEM: LINKED LIST CYCLE Created files

---
+++ Easy-Practice/src/main/java/linked_list_cycle/ListNode.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linked_list_cycle;
+
+/**
+ *
+ * @author Rsand
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+        next = null;
+    }
+}
