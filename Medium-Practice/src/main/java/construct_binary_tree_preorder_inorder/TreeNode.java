6f5ed625739c96e7cfcfc70a2832361d6e74a2ec
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-20 14:54:32 -0400

    NEW MEDIUM PROBLEM: CONSTRUCT BINARY TREE FROM PREORDER AND INORDER TRAVERSAL Created files

---
+++ Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/TreeNode.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package construct_binary_tree_preorder_inorder;
+
+/**
+ *
+ * @author Rsand
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
