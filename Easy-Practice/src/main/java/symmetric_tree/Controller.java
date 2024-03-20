ff4c16f3cbca76aba0b419d267f4e7bed6bb4788
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-20 14:19:00 -0400

    SYMMETRIC TREE Added test code to Controller

--- Easy-Practice/src/main/java/symmetric_tree/Controller.java
+++ Easy-Practice/src/main/java/symmetric_tree/Controller.java
@@ -10,6 +10,15 @@ package symmetric_tree;
  */
 public class Controller {
     public static void main(String[] args) {
+        TreeNode node7 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(2,node6,node7);
+        TreeNode node2 = new TreeNode(2,node4,node5);
+        TreeNode root  = new TreeNode(1,node2, node3);
         
+        Solution sol = new Solution();
+        boolean symmetricTree = sol.isSymmetric(root);
     }
 }
