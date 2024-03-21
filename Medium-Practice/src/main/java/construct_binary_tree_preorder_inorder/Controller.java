9ca96ee0d083d62c34e72ed51d6c26bfec9ac579
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-21 14:37:08 -0400

    NEW MEDIUM PROBLEM: CONSTRUCT BINARY TREE FROM PREORDER AND INORDER TRAVERSAL Added arraylists to pass as preorder and inorder arrays for subtrees in recursive way. Added while loops to create inorder lists

--- Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/Controller.java
+++ Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/Controller.java
@@ -10,6 +10,14 @@ package construct_binary_tree_preorder_inorder;
  */
 public class Controller {
     public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        int[] preorder = {3,9,20,2,8,15,7,5,6,4,1,16};
+        int[] inorder = {5,2,9,8,3,6,15,4,20,1,7,16};
+        
+        TreeNode binaryTree = sol.buildTree(preorder, inorder);
+ 
+        
         
     }
 }
--- Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/Solution.java
+++ Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/Solution.java
@@ -4,6 +4,9 @@
  */
 package construct_binary_tree_preorder_inorder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Rsand
@@ -11,5 +14,31 @@ package construct_binary_tree_preorder_inorder;
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder){
         
-    }
+        List<Integer> leftPreOrder = new ArrayList<Integer>();
+        List<Integer> leftInOrder = new ArrayList<Integer>();
+        List<Integer> rightPreOrder = new ArrayList<Integer>();
+        List<Integer> rightInOrder = new ArrayList<Integer>();
+        
+        int root = preorder[0];
+        int leftChild, rightChild;
+        
+        // Create inorder list for left subtree
+        int idx = 0;
+        while (inorder[idx] != root){
+            leftInOrder.add(inorder[idx]);
+            idx++;
+        }
+        
+        // Skip root node
+        idx++;
+        
+        // Create inorder list for right subtree
+        while (idx < inorder.length){
+            rightInOrder.add(inorder[idx]);
+            idx++;
+        }
+        
+        return null;
+        //buildTree();
+        }
 }
