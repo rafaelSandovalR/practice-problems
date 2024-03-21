32e87c316dab61c39074b2cba3f950ee2de7246d
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-21 15:15:49 -0400

    CONSTRUCT BINARY TREE FROM PREORDER AND INORDER TRAVERSAL Added for loop to create preorder lists. Added arraylist to array conversions. Added recursive return call.

--- Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/Solution.java
+++ Medium-Practice/src/main/java/construct_binary_tree_preorder_inorder/Solution.java
@@ -5,7 +5,7 @@
 package construct_binary_tree_preorder_inorder;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  *
@@ -14,10 +14,12 @@ import java.util.List;
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder){
         
-        List<Integer> leftPreOrder = new ArrayList<Integer>();
-        List<Integer> leftInOrder = new ArrayList<Integer>();
-        List<Integer> rightPreOrder = new ArrayList<Integer>();
-        List<Integer> rightInOrder = new ArrayList<Integer>();
+        if (preorder.length == 0) return null;
+        
+        ArrayList<Integer> leftPreOrder = new ArrayList<Integer>();
+        ArrayList<Integer> leftInOrder = new ArrayList<Integer>();
+        ArrayList<Integer> rightPreOrder = new ArrayList<Integer>();
+        ArrayList<Integer> rightInOrder = new ArrayList<Integer>();
         
         int root = preorder[0];
         int leftChild, rightChild;
@@ -38,7 +40,30 @@ public class Solution {
             idx++;
         }
         
-        return null;
-        //buildTree();
+        // Create preorder lists for both branches
+        for (int i = 1; i < preorder.length; i++){
+            if (leftInOrder.contains(preorder[i])){
+                leftPreOrder.add(preorder[i]);
+            } else {
+                rightPreOrder.add(preorder[i]);
+            }
+        }
+        
+        // Convert ArrayLists to arrays
+        int[] leftpreorder = new int[leftPreOrder.size()];
+        Arrays.setAll(leftpreorder, leftPreOrder::get);
+        
+        int[] leftinorder = new int[leftInOrder.size()];
+        Arrays.setAll(leftinorder, leftInOrder::get);
+        
+        int[] rightpreorder = new int[rightPreOrder.size()];
+        Arrays.setAll(rightpreorder, rightPreOrder::get);
+        
+        int[] rightinorder = new int[rightInOrder.size()];
+        Arrays.setAll(rightinorder, rightInOrder::get);
+        
+        
+        
+        return new TreeNode(root, buildTree(leftpreorder,leftinorder),buildTree(rightpreorder,rightinorder));
         }
 }
