c3a1fb6ac8cc3ef27881b191cc05782cc8c64afd
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-23 15:51:45 -0400

    NEW MEDIUM PROBLEM: BINARY TREE RIGHT SIDE VIEW Created files

--- Medium-Practice/src/main/java/binary_tree_right_side_view/Controller.java
+++ Medium-Practice/src/main/java/binary_tree_right_side_view/Controller.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package construct_binary_tree_preorder_inorder;
+package binary_tree_right_side_view;
 
 /**
  *
@@ -10,14 +10,6 @@ package construct_binary_tree_preorder_inorder;
  */
 public class Controller {
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] preorder = {9,1,2,4,7,3,8,11,6,13,15,14,5,10,12};
-        int[] inorder = {11,4,6,1,7,10,13,12,9,15,3,14,2,8,5};
-
-        
-        TreeNode binaryTree = sol.buildTree(preorder, inorder);
- 
-        
         
     }
 }
---
+++ Medium-Practice/src/main/java/binary_tree_right_side_view/Solution.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package binary_tree_right_side_view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root){
+        List<Integer> rightSide = new ArrayList<Integer>();
+        
+        
+    }
+}
