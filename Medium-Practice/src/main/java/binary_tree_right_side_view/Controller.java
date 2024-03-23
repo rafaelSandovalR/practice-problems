66db33a0586df14b85326fa71a80d4e8f58b9d36
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-23 16:21:07 -0400

    NEW MEDIUM PROBLEM: BINARY TREE RIGHT SIDE VIEW Added test code to controller

--- Medium-Practice/src/main/java/binary_tree_right_side_view/Controller.java
+++ Medium-Practice/src/main/java/binary_tree_right_side_view/Controller.java
@@ -4,12 +4,21 @@
  */
 package binary_tree_right_side_view;
 
+import java.util.List;
+
 /**
  *
  * @author Rsand
  */
 public class Controller {
     public static void main(String[] args) {
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2, node4,null);
+        TreeNode root = new TreeNode(1,node2,node3);
         
+        Solution sol = new Solution();
+        
+        List<Integer> rightSideView = sol.rightSideView(root);
     }
 }
