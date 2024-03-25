a097058c4193b1735afb863fe25e7c7506b44f09
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-25 14:32:25 -0400

    AVERAGE OF LEVELS IN BINARY TREE Added test code

--- Easy-Practice/src/main/java/average_levels_binary_tree/Controller.java
+++ Easy-Practice/src/main/java/average_levels_binary_tree/Controller.java
@@ -4,13 +4,22 @@
  */
 package average_levels_binary_tree;
 
+import java.util.List;
+
 /**
  *
  * @author Rsand
  */
 public class Controller {
     public static void main(String[] args) {
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node3 = new TreeNode(20,node4,node5);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode root = new TreeNode(3, node2, node3);
         
+        Solution sol = new Solution();
+        List<Double> answer = sol.averageOfLevels(root);
     }
    
 }
