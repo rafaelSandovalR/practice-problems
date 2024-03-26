8ba93ce0ece5266a4f74e411faf077e58c362306
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-26 16:14:18 -0400

    NEW EASY PROBLEM: MINIMUM ABSOLUTE DIFFERENCE IN BST Created files and added solution

---
+++ Easy-Practice/src/main/java/min_absolute_difference_bst/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package min_absolute_difference_bst;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    int min = Integer.MAX_VALUE;
+    Integer prev = null;
+    
+    public int getMinimumDifference(TreeNode root){
+        
+        if (root == null)  return min;
+        
+        // Visit all smaller nodes first
+        getMinimumDifference(root.left);
+        
+        // If it's visited at least one node
+        if (prev != null) {
+            min = Math.min(min, root.val - prev);
+        }
+        
+        // Visit all larger nodes
+        prev = root.val;
+
+        getMinimumDifference(root.right);
+
+        return min;
+    }
+    
+}
