bf54fc5522d5f7e9e8345c1eb5d6d9c4a391f5c8
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-29 16:47:31 -0400

    NUMBER OF ISLANDS Added code to create new 'island' within each row. Added comments with necessary steps.

--- Medium-Practice/src/main/java/number_of_islands/Solution.java
+++ Medium-Practice/src/main/java/number_of_islands/Solution.java
@@ -15,12 +15,23 @@ public class Solution {
         int rows = grid.length;
         int columns = grid[0].length;
         
-        ArrayList<Integer[]> islands = new ArrayList<Integer[]>();
+        ArrayList<int[]> islands = new ArrayList<int[]>();
         
         for (int row = 0; row < rows; row++){
-         
             for (int col = 0; col < columns; col++){
-                
+                // Determine land ranges within row
+                if (grid[row][col] == '1') {
+                    int[] island = new int[3]; // Index 0 = last row updated, Index 1 = range min, Index 2 = range max
+                    island[0] = row;
+                    island[1] = col;
+                    while (grid[row][col] == '1') {
+                        col++;
+                    }
+                    island[2] = col;
+                }
+                // Determine if land connects to prexisting land
+                // If so, update island range and last row updated
+                // If not, add new island to list
             }
         }
         
