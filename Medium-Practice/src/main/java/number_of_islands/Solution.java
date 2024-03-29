7820ffa4f36017b0277eb372a5c4a278b25bd0c4
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-29 16:30:26 -0400

    NEW MEDIUM PROBLEM: NUMBER OF ISLANDS Created files. Added ArrayList to hold integer arrays to hold island ranges Added nested loops to traverse grid.

---
+++ Medium-Practice/src/main/java/number_of_islands/Solution.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package number_of_islands;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int numIslands(char[][] grid){
+        int rows = grid.length;
+        int columns = grid[0].length;
+        
+        ArrayList<Integer[]> islands = new ArrayList<Integer[]>();
+        
+        for (int row = 0; row < rows; row++){
+         
+            for (int col = 0; col < columns; col++){
+                
+            }
+        }
+        
+        return islands.size();
+    }
+}
