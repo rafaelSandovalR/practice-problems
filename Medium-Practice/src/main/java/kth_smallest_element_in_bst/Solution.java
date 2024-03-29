71400bf122eb9fdf4b47318b43219cac303a9901
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-03-28 22:44:12 -0400

    NEW MEDIUM PROBLEM: KTH SMALLEST ELEMENT IN A BST Created files

---
+++ Medium-Practice/src/main/java/kth_smallest_element_in_bst/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kth_smallest_element_in_bst;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int kthSmallest(TreeNode root, int k){
+        
+    }
+}
