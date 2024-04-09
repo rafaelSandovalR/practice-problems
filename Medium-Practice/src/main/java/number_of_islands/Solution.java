cbb3aac0fe4ce7efeb3b4d363d53d4e917708aa6
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-04-08 20:17:22 -0400

    NUMBER OF ISLANDS Removed code for new solution attempt.

--- Medium-Practice/src/main/java/number_of_islands/Solution.java
+++ Medium-Practice/src/main/java/number_of_islands/Solution.java
@@ -14,55 +14,30 @@ public class Solution {
     public int numIslands(char[][] grid){
         int rows = grid.length;
         int columns = grid[0].length;
-        
-        ArrayList<int[]> islands = new ArrayList<int[]>();
-        
+        int islandCount = 0;
+               
         for (int row = 0; row < rows; row++){
-            int islandsInRow = 0;
+            ArrayList<int[]> currentIslands = new ArrayList<int[]>();
             for (int col = 0; col < columns; col++){
                 // Determine land ranges within row
                 if (grid[row][col] == '1') {
-                    boolean connected = false;
-                    int[] island = new int[3];
-                    // Set last row updated
-                    island[0] = row;
+                    int[] island = new int[2];
                     // Set Width Range Min Boundary
-                    island[1] = col;
+                    island[0] = col;
                     // Determine width of current island within current row
                     while (col+1 < columns && grid[row][col+1] == '1') {
                         col++;
                     }
                     // Set Width Range Max Boundary
-                    island[2] = col;
+                    island[1] = col;
                     
-                    // Add island if none exist or if last island isn't from previous row
-                    if (islands.isEmpty() || (islands.size()-1-islandsInRow > 0 && islands.get(islands.size()-1-islandsInRow)[0] != row - 1)){
-                        islands.add(island);
-                        islandsInRow++;
-                    }
-                    // Determine if land connects to prexisting land
-                    else {
-                        // Go through list in reverse for all islands found in the previous row
-                        for (int i = islands.size() - islandsInRow - 1; i >= 0 && islands.get(i)[0] == row - 1; i--) {
-                            int[] islandInList = islands.get(i);
-                            if (islandsConnect(island[1], island[2], islandInList[1], islandInList[2])) {
-                                // Move updated island edge to the end of the list
-                                islands.remove(i);
-                                islands.add(island);
-                                connected = true;
-                                islandsInRow++;
-                            } 
-                        } 
-                        if (!connected){
-                            islands.add(island);
-                            islandsInRow++;
-                        }
-                    }                    
-                }  
+                    currentIslands.add(island);
+                }
+                
             }
         }
         
-        return islands.size();
+        return islandCount;
     }
 
     private boolean islandsConnect(int firstMin, int firstMax, int secondMin, int secondMax) {
