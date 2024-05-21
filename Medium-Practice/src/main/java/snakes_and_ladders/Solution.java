f6a708ed882ec0675d6471aa5f5dada57bbcd976
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-05-21 14:51:24 -0400

    Replaced all code with new solution using a queue and nested loops.

--- Medium-Practice/src/main/java/snakes_and_ladders/Solution.java
+++ Medium-Practice/src/main/java/snakes_and_ladders/Solution.java
@@ -4,9 +4,8 @@
  */
 package snakes_and_ladders;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  *
@@ -14,77 +13,32 @@ import java.util.Set;
  */
 public class Solution {
     public int snakesAndLadders(int[][] board){
-        
-        int[][] memo = new int[board.length][board.length];
-        for (int i = 0; i < memo.length; i++){
-            for (int j = 0; j < memo.length; j++){
-                memo[i][j] = -1;
-            }
-        }
-        
-        return minMoves(board, 1, memo, new HashSet<>());
-    }
-    
-    private int minMoves(int[][] board, int curr, int[][] memo, Set<Integer> visited){
         int n = board.length;
-        if (curr == n * n){
-            return 0;
-        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        boolean[] visited = new boolean[n * n + 1];
         
-        // Check memoization table
-        int[] coordinates = getCoordinates(curr, n);
-        int row = coordinates[0];
-        int col = coordinates[1];
-        if (memo[row][col] != -1){
-            return memo[row][col];
-        }
-        
-        // Initialize min moves
-        int minMoves = Integer.MAX_VALUE;
-        for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++){
+        for (int move = 0; !queue.isEmpty(); move++){
             
-            if (visited.contains(next)) continue; // Skip visited squares to avoid cycles
-            visited.add(next); // Mark the next square as visited
-            
-            int[] nextCoordinates = getCoordinates(next, n);
-            int nextRow = nextCoordinates[0];
-            int nextCol = nextCoordinates[1];
-            
-            // Check for snake or ladder
-            if (board[nextRow][nextCol] != -1){
-                minMoves = Math.min(minMoves, 1 + minMoves(board, board[nextRow][nextCol], memo, visited));
-            } else{
-                minMoves = Math.min(minMoves, 1 + minMoves(board, next, memo, visited));
+            for (int size = queue.size(); size > 0; size--){
+                
+                int num = queue.poll();
+                if (visited[num]) continue;
+                visited[num] = true;
+                if (num == n * n) return move;
+                
+                for (int i = 1; i <= 6 && num + i <= n * n; i++){
+                    int next = num + i;
+                    int value = getBoardValue(board, next);
+                    if (value > 0) next = value;
+                    if (!visited[next]) queue.offer(next);
+                }
             }
-            
-            visited.remove(next); // Remove the square for visited after exploring its possibilites
-        }
-        
-        // Store minimum moves for current square
-        memo[row][col] = minMoves;
-        
-        return minMoves;
-    }
-    
-    private int[] getCoordinates(int curr, int n){
-        // Get canonical row & col
-        int r = curr / n;
-        int c = curr % n;
-        
-        // Get actual row based on Boustrophedon style & if remainder exists
-        int row = c != 0 ? (n - 1) - r : n - r;
-        
-        int col;
-        // Get actual column based on even/odd row & if remainder exists
-        if (c == 0){
-            // If no remainder, it's either the first or last column
-            col = r % 2 == 0 ? 0 : n - 1;
-        } else {
-            col = r % 2 == 0 ? c - 1 : n - c;
         }
 
-        //System.out.println("Coordinates : [" + row + "] [" + col + "]");
-        return new int[]{row, col};
-    }    
+        return -1;
+    }
+    
+   
 
 }
