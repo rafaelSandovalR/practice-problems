523291e3cb4599076be54eebbc065873d3133d10
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-05-23 15:13:23 -0400

    BFS LINEAR SOLUTION: Added 2D to linear conversion. Added circular queue variables.

--- Medium-Practice/src/main/java/snakes_and_ladders/BFS_Linear_Solution.java
+++ Medium-Practice/src/main/java/snakes_and_ladders/BFS_Linear_Solution.java
@@ -14,23 +14,41 @@ public class BFS_Linear_Solution {
         final int endSquare = n * n;
         
         /**
-         * Convert the Boustrophedon orderd 2-D board to a simple
+         * Convert the Boustrophedon ordered 2-D board to a simple
          * linear 1-D array indexed by square number. Nothing in 
          * the game requires a 2-D board.
          */
         
         short[] brd = new short[endSquare + 1];
-        int brdIdx = 1;
+        int brdIdx = 1; // Tracks position in the linear array where to store next value
         
-        for (int row = n-1; row >= 0; row--){
-            for (int col = 0; col < n ; col++){
-                brd[brdIdx++] = (short)board[row][col];
-                if (--row < 0) break;
-                
+        // Convert to linear array, start with last row
+        for (int row = n - 1; row >= 0; row--) {
+            // Go through board columns from left to right
+            for (int col = 0; col < n; col++) {
+                brd[brdIdx++] = (short) board[row][col]; // Cast int board value to short
             }
+            
+            if (--row < 0) break; // Move to next row and break if previous was last row
+
+            // Go through even rows in reverse
             for (int col = n - 1; col >= 0; col--) {
                 brd[brdIdx++] = (short) board[row][col];
             }
         }
+        
+        /**
+         * Circular queue for the BFS. If the queue is empty
+         * (bfsQueueRead == bfsQueueWrite), then there are no more possible
+         * moves in the game. Each entry in the queue is the square number of
+         * the square to make moves from.
+         */
+        
+        final int bfsQueueLen = Math.min(n * n, 8 * n);
+        short[] bfsQueue = new short[bfsQueueLen]; // Queue for Squares to be explored
+        int bfsQueueRead = 0; // Keeps track of the index from which the next element will be read from the queue
+        int bfsQueueWrite = 0; // Keeps track of the index at which the next element will be added to the queue
+        
+        bfsQueue[bfsQueueWrite++] = 1; // Adds starting square
     }
 }
