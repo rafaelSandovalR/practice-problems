20d2d05221af4d48bcaf34398607001da03308f8
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-05-24 13:59:18 -0400

    NEW MEDIUM PROBLEM: IMPLEMENT TRIE Created classes and file

---
+++ Medium-Practice/src/main/java/implement_trie/Trie.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implement_trie;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Trie {
+    public Trie(){
+        
+    }
+    
+    public void insert(String word){
+    }
+    
+    public boolean search(String word){
+        
+    }
+    
+    public boolean startsWith(String prefix){
+        
+    }
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
