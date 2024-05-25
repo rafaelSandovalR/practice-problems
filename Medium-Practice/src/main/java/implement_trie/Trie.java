3fe7a64d534e2e546a51c6df50d492e1b6d54d69
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-05-25 12:39:53 -0400

    NEW MEDIUM PROBLEM: IMPLEMENT TRIE Added code to each Trie class method

--- Medium-Practice/src/main/java/implement_trie/Trie.java
+++ Medium-Practice/src/main/java/implement_trie/Trie.java
@@ -5,6 +5,7 @@
 package implement_trie;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -12,35 +13,70 @@ import java.util.List;
  * @author Rsand
  */
 public class Trie {
-    node root = new node();
+    Node root;
     
     public Trie(){
-        
+        root = new Node(' ');
     }
     
     public void insert(String word){
-        
+        char[] charArray = word.toCharArray();
+        Node currentNode = root;
+
+        for (int i = 0; i < charArray.length; i++){
+            char charKey = charArray[i];
+            if (currentNode.childrenMap.containsKey(charKey)){
+                currentNode = currentNode.childrenMap.get(charKey);
+            } else{
+                Node newChild = new Node(charKey);
+                currentNode.addChild(newChild);
+            }
+        }
+        currentNode.isWord = true;
     }
     
     public boolean search(String word){
+        char[] charArray = word.toCharArray();
+        Node currentNode = root;
         
+        for(int i = 0; i < charArray.length; i++){
+            char charKey = charArray[i];
+            if (!currentNode.childrenMap.containsKey(charKey)){
+                return false;
+            }
+            currentNode = currentNode.childrenMap.get(charKey);
+        }
+        
+        return currentNode.isWord;
     }
     
     public boolean startsWith(String prefix){
-        
+        char[] charArray = prefix.toCharArray();
+        Node currentNode = root;
+
+        for (int i = 0; i < charArray.length; i++) {
+            char charKey = charArray[i];
+            if (!currentNode.childrenMap.containsKey(charKey)) {
+                return false;
+            }
+            currentNode = currentNode.childrenMap.get(charKey);
+        }
+        return true;
     }
     
-    private class node{
-        boolean[] contains;
-        List<node> children;
+    private class Node{
+        char letter;
+        boolean isWord;
+        HashMap<Character, Node> childrenMap;
         
-        public node(){
-            contains = new boolean[27]; // index 15 stands for * and the rest are lower case letters % 27
-            children = new ArrayList<>();
+        public Node(char letter){
+            this.isWord = false;
+            this.letter = letter;
+            this.childrenMap = new HashMap<>();
         }
         
-        public void addChild(node child){
-            children.add(child);
+        public void addChild(Node child){
+            childrenMap.put(letter, child);
         }
         
         
