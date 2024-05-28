649baee335a457de6a7e958423ba8aa0be21718d
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-05-28 15:59:18 -0400

    NEW MEDIUM PROBLEM: IMPLEMENT TRIE Added test code to the controller

--- Medium-Practice/src/main/java/implement_trie/Controller.java
+++ Medium-Practice/src/main/java/implement_trie/Controller.java
@@ -4,10 +4,24 @@
  */
 package implement_trie;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rsand
  */
 public class Controller {
-    
+    public static void main(String[] args) {
+        
+       Trie trie = new Trie();
+       trie.insert("apple");
+       boolean result1 = trie.search("apple");
+       boolean result2 = trie.search("app");
+       boolean result3 = trie.startsWith("app");
+       trie.insert("app");
+       boolean result4 = trie.search("app");
+       
+        
+
+    }
 }
