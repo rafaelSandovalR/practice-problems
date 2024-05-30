77f926a6f494bdbc4ef593010198696601ed53e0
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-05-30 14:26:38 -0400

    SORT LINKED LIST Added an insertion sort solution.

--- Medium-Practice/src/main/java/sort_linked_list/Solution.java
+++ Medium-Practice/src/main/java/sort_linked_list/Solution.java
@@ -10,6 +10,30 @@ package sort_linked_list;
  */
 public class Solution {
     public ListNode sortList(ListNode head){
+        if (head == null || head.next == null) return head;
         
+        ListNode current = head;
+        
+        while(current != null){
+            ListNode minNode = current; // Assume current is min initially 
+            ListNode search = current.next; // Pointer to search for smallest node
+            
+            while (search != null){
+                if (search.val < minNode.val){
+                    minNode = search; // Update min
+                }
+                search = search.next;
+            }
+            
+            // Swap values in current and minNode
+            int temp = current.val;
+            current.val = minNode.val;
+            minNode.val = temp;
+            
+            current = current.next; // Move to the next unsorted node
+        }
+        
+        
+        return head;
     }
 }
