3619b2a240bb406b9b77a85af5f75ab39d621bad
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-04 14:46:31 -0400

    NEW MEDIUM PROBLEM: KTH LARGEST ELEMENT IN ARRAY Created classes/file Added starter code with array as minheap

---
+++ Medium-Practice/src/main/java/kth_largest_in_array/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kth_largest_in_array;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int findKthLargest(int[] nums, int k){
+        
+        int[] minHeap = new int[k]; 
+        int heapSize = 0;
+        
+        for (int i = 0; i < nums.length; i++){
+            if (heapSize < k){
+                
+            }
+        }
+        
+        return minHeap[0];
+}
