cc53ce18ef720d39f5d81ad7da2b0b1b7ed8fe9a
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-04 15:52:27 -0400

    KTH LARGEST ELEMENT IN ARRAY Created method to insert values into heap in proper place

--- Medium-Practice/src/main/java/kth_largest_in_array/Solution.java
+++ Medium-Practice/src/main/java/kth_largest_in_array/Solution.java
@@ -13,14 +13,41 @@ import java.util.Arrays;
 public class Solution {
     public int findKthLargest(int[] nums, int k){
         
-        int[] minHeap = new int[k]; 
-        int heapSize = 0;
+        int[] minHeap = new int[k];
+        
+        for (int i = 0; i < k; i++) {
+            minHeap[i] = Integer.MIN_VALUE;
+        }
+        
+        
+        for (int i = 0; i < k; i++){
+            insert(nums, nums[i]);
+        }
         
         for (int i = 0; i < nums.length; i++){
-            if (heapSize < k){
+            if (nums[i] < minHeap[0]){
                 
             }
+            
+            
+            
         }
         
         return minHeap[0];
+    }
+    
+    private void insert(int[] minHeap, int val){
+        int idx = minHeap.length - 1;
+        minHeap[idx] = val;
+        int parentIdx = (idx - 1) / 2;
+
+        while (idx >= 0 && val < minHeap[parentIdx]){
+            int temp = minHeap[parentIdx];
+            minHeap[parentIdx] = val;
+            minHeap[idx] = temp;
+            
+            idx = parentIdx;
+            parentIdx = (idx - 1) / 2;
+        }
+    }
 }
