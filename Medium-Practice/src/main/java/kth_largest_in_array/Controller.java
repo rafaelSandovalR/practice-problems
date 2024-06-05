a8351d70d96ba452c383ee965addbf8c83b942f1
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-05 15:31:43 -0400

    KTH LARGEST ELEMENT IN ARRAY Created method to remove first(smallest) element from heap array. Created method to return the index of smallest child of an index.

--- Medium-Practice/src/main/java/kth_largest_in_array/Controller.java
+++ Medium-Practice/src/main/java/kth_largest_in_array/Controller.java
@@ -10,6 +10,9 @@ package kth_largest_in_array;
  */
 public class Controller {
     public static void main(String[] args) {
+        Solution sol = new Solution();
         
+        int[] nums = {3,2,1,5,6,4};
+        int ans = sol.findKthLargest(nums, 4);
     }
 }
--- Medium-Practice/src/main/java/kth_largest_in_array/Solution.java
+++ Medium-Practice/src/main/java/kth_largest_in_array/Solution.java
@@ -16,21 +16,24 @@ public class Solution {
         int[] minHeap = new int[k];
         
         for (int i = 0; i < k; i++) {
-            minHeap[i] = Integer.MIN_VALUE;
+            minHeap[i] = Integer.MAX_VALUE;
         }
         
         
         for (int i = 0; i < k; i++){
-            insert(nums, nums[i]);
+            insert(minHeap, nums[i]);
         }
         
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] < minHeap[0]){
-                
+        //test
+        minHeap[2] = 3;
+        //
+        
+        for (int i = k; i < nums.length; i++){
+            if (nums[i] > minHeap[0]){
+                remove(minHeap);
+                insert(minHeap, nums[i]);
             }
-            
-            
-            
+
         }
         
         return minHeap[0];
@@ -49,5 +52,37 @@ public class Solution {
             idx = parentIdx;
             parentIdx = (idx - 1) / 2;
         }
+    }
+    
+    private void remove(int[] minHeap){
+        int heapSize = minHeap.length;
+        int idx = 0;
+        minHeap[idx] = minHeap[heapSize - 1];
+        int smallestChildIdx = 0;
+        
+        while (smallestChildIdx >= 0){
+            smallestChildIdx = getSmallestChildIdx(minHeap, idx);
+            if (smallestChildIdx < 0) break;
+            int temp = minHeap[idx];
+            minHeap[idx] = minHeap[smallestChildIdx];
+            minHeap[smallestChildIdx] = temp;
+            idx = smallestChildIdx;
+        }
+    }
+    
+    private int getSmallestChildIdx(int[] minHeap, int idx){
+        int size = minHeap.length - 1;
+        int leftChild = idx * 2 + 1;
+        int rightChild = idx * 2 + 2;
+
+        
+        if (leftChild >= size && rightChild >= size){
+            return -1;
+        }
+        
+        if (rightChild >= size) return leftChild;
+        else if (leftChild >= size) return rightChild;
+        else if (minHeap[leftChild] > minHeap[rightChild]) return rightChild;
+        else return leftChild;
     }
 }
