bc8769c2b3c9f59c99882be267329933c735b574
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-07 15:45:08 -0400

    NEW EASY PROBLEM: ADD BINARY Replaced code with more concise solution converting chars to ints

--- Easy-Practice/src/main/java/add_binary/Solution.java
+++ Easy-Practice/src/main/java/add_binary/Solution.java
@@ -10,64 +10,35 @@ package add_binary;
  */
 public class Solution {
     public String addBinary(String a, String b){
-          
         int aPtr = a.length() - 1;
         int bPtr = b.length() - 1;
         StringBuilder sum = new StringBuilder();
-        char carry = '0';
+        int carry = 0;
         
-        while (aPtr >= 0 && bPtr >= 0){
-            char[] result = add(a.charAt(aPtr--), b.charAt(bPtr--), carry);
-            sum.append(result[0]);
-            carry = result[1];
+        while (aPtr >=0 || bPtr >=0){
+            int sumDigit = carry;
+            if (aPtr >= 0) sumDigit += a.charAt(aPtr--) - '0'; // Convert char to int
+            if (bPtr >= 0) sumDigit += b.charAt(bPtr--) - '0';
+            sum.append(sumDigit % 2);
+            carry = sumDigit / 2;
+            /*
+                +-------+---+---+---+
+                |   a   | 1 | 1 | 1 |
+                |   b   | 0 | 1 | 1 |
+                | carry | 0 | 0 | 1 |
+                |-------------------|
+                | TOTAL | 1 | 2 | 3 |
+                +-------+---+---+---+
+                | %2=Sum| 1 | 0 | 1 |
+                | /2=Cry| 0 | 1 | 1 |
+                +-------+---+---+---+
+            
+            */
         }
         
-        // Logic when a and b are not the same length
-        if (aPtr >=0 || bPtr >= 0){
-            int remPtr = aPtr >= 0 ? aPtr : bPtr;
-            String z = aPtr >= 0 ? a : b;
-
-            while (remPtr >= 0) {
-                char[] result = add(z.charAt(remPtr--), carry, '0');
-                sum.append(result[0]);
-                carry = result[1];
-            }
-        }
-        
-        // If carry remains
-        if (carry == '1'){
-            sum.append(carry);
-        }
-
+        if (carry == 1) sum.append(carry);
         
         return sum.reverse().toString();
-        
     }
     
-    private char[] add(char a, char b, char carry){
-        char[] result = {'0','0'}; // [sum, carry];
-        
-        // Logic for a + b calculation
-        if (a == '1' && b == '1') {
-            result[0] = '0';
-            result[1] = '1';
-        } else if ( a == '1' || b == '1'){
-            result[0] = '1';
-        }
-        
-        // Logic if carry is brought over from before
-        if (carry == '1'){
-            switch (result[0]) {
-                case '0':
-                    result[0] = '1';
-                    break;
-                case '1':
-                    result[0] = '0';
-                    result[1] = '1';
-                    break;
-            }
-        }
-        
-        return result;
-    }
 }
