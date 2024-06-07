a7d721bc88d945ce6de8e553b9a7fbcc5b49045b
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-07 15:00:06 -0400

    NEW EASY PROBLEM: ADD BINARY Created classes and solved problem

---
+++ Easy-Practice/src/main/java/add_binary/Controller.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package add_binary;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        String a = "1111";
+        String b = "1111";
+        
+        Solution sol = new Solution();
+        
+        String ans = sol.addBinary(a, b);
+    }
+}
