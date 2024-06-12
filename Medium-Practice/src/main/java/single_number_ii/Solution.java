5edd8925366ced5e637e9a6222eedbeba1e3a0ce
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-12 16:51:06 -0400

    SINGLE NUMBER II Removed the customLog method: Only works for positive numbers Changed loop to go through all 32 array indices Added logic to convert count array into decimal value

--- Medium-Practice/src/main/java/single_number_ii/Solution.java
+++ Medium-Practice/src/main/java/single_number_ii/Solution.java
@@ -12,27 +12,36 @@ public class Solution {
     public int singleNumber(int[] nums){
         
         int[] count = new int[32];
-        
-        for (int num : nums){
-            count(count, num);
+
+        for (int num : nums) {
+            getCount(count, num);
         }
 
         int ans = 0;
+
+        for (int i = 31; i >= 0 ; i--) {
+            ans <<= 1;
+            count[i] = count[i] % 3;
+            ans += count[i]; 
+        }
+
         return ans;
     }
-    
-    public void count(int[] count, int n) {
-        int log = customLog(2, n);
-        
-        for (int i = 0; i <= log; i++) {
+
+    public void getCount(int[] count, int n) {
+
+        for (int i = 0; i < 32; i++) {
 
             count[i] += n & 1; // If rightmost bit is 1, add 1 to result
             n >>= 1; // Shift n to the right to get the next digit
         }
-        
+
     }
+
+    /*  Only works for positive numbers
     
-    private static int customLog(int base, int logNumber){
-        return (int)(Math.log(logNumber) / Math.log(base));
+    private static int customLog(int base, int logNumber) {
+        return (int) (Math.log(logNumber) / Math.log(base));
     }
+    */
 }
