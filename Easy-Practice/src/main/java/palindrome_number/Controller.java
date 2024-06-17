45d01db91104968e192d459fdde199f570ab483b
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-17 13:05:51 -0400

    NEW EASY PROBLEM: PALINDROME NUMBER Created classes and added initial solution attempt

---
+++ Easy-Practice/src/main/java/palindrome_number/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package palindrome_number;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int num = 1390931;
+        boolean ans = sol.isPalindrome(num);
+    }
+}
---
+++ Easy-Practice/src/main/java/palindrome_number/Solution.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package palindrome_number;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public boolean isPalindrome(int x){
+        if (x < 0) return false;
+        if (x <= 9) return true;
+        
+        // Figure out the most significant factor
+        int high = 0;
+        for (int i = 1, res = x; res > 9; i++){
+            res /= 10;
+            high = i;
+        }
+        
+        high = (int)Math.pow(10, high);
+        
+        // Check for palindrome from both ends
+        
+        for (int low = 10, i = 1; low <= high; low *= 10, high /= 10, i*=10){
+            int front = (x % low) / i;
+            int back = Math.max(x/high, (x/high) % i);
+            
+            if (front != back) return false;
+        }
+        
+        return true;
+    }
+}
