fddffb5c61af23c9d1c7aa7ed9412f8e666eaff0
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-06-17 12:18:25 -0400

    SINGLE NUMBER II Added comments to alternate solution

--- Medium-Practice/src/main/java/single_number_ii/Solution_2.java
+++ Medium-Practice/src/main/java/single_number_ii/Solution_2.java
@@ -10,15 +10,28 @@ package single_number_ii;
  */
 public class Solution_2 {
     public int singleNumber(int[] nums){
-        int ones = 0;
-        int twos = 0;
+        int ones = 0; // Track bits that have appeared once (modulo 3)
+        int twos = 0; // Track bits that have appeared twice (modulo 3)
         int n = nums.length;
         
+        
         for (int i =0; i < n; i++){
+            /*
+            - XOR ones with nums[i] to toggle bits
+            - Bitwise AND with ~twos to reset bits in 'ones' if they were 
+                already in 'twos' (appeared twice)
+            */
             ones = (ones ^ nums[i]) & ~(twos);
+            
+            /*
+            - Update 'twos' based on the new value of 'ones'
+            - XOR twos with nums[i] to toggle bits
+            - Bitwise AND with ~ones to reset bits in 'twos' if they were
+                already in 'ones' (appeared three times)
+            */
             twos = (twos ^ nums[i]) & ~(ones);
         }
         
-        return ones;
+        return ones; // Return the bits that have appeared exactly once (the single number)
     }
 }
