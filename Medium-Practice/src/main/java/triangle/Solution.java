ad5121da3376e24f14c2143de22f9a785e5ccffa
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-07-12 23:30:22 -0400

    TRIANGLE Replaced code with start of new solution using an int array to apply dynamic programming

--- Medium-Practice/src/main/java/triangle/Solution.java
+++ Medium-Practice/src/main/java/triangle/Solution.java
@@ -12,23 +12,14 @@ import java.util.List;
  */
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle){
-        int min = triangle.get(0).get(0);
         
-        if (triangle.size() == 1) return min;
-        
-        for (int row = 1, prevCol = 0; row < triangle.size(); row++){
-            int option1 = triangle.get(row).get(prevCol);
-            int option2 = triangle.get(row).get(prevCol + 1);
-            
-            if (option1 < option2){
-                min += option1;
-            } else{
-                min += option2;
-                prevCol++;
-            }
-            
+        int size = 0;
+        for (List list : triangle){
+            size += list.size();
         }
+        int[] dp = new int[size];
         
-        return min;
+        
+        
     }
 }
