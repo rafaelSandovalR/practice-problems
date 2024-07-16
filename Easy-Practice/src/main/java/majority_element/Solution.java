a6e9adc183ee8c83fa2ab3eab9d837d3df856110
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-07-16 13:49:44 -0400

    MAJORITY ELEMENT Added Boyer-Moore Voting Algorithm with O(n) time complexity

--- Easy-Practice/src/main/java/majority_element/Solution.java
+++ Easy-Practice/src/main/java/majority_element/Solution.java
@@ -14,6 +14,7 @@ public class Solution {
     public int majorityElement(int[] nums){
 /**
  * ORIGINAL SOLUTION
+ * TIME COMPLEXITY: O(n log n)
          * Arrays.sort(nums);
          *
          * int count = 1; int ptr = 0; int majoritySize = (nums.length / 2) + 1;
@@ -26,7 +27,36 @@ public class Solution {
          * return nums[ptr];
  */
 
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+/**
+ * CONCISE SOLUTION: 
+ * TIME COMPLEXITY: O(n log n)
+         * Arrays.sort(nums); return nums[nums.length / 2];
+ */
+
+/**
+ * BOYER-MOORE VOTING ALGORITHM
+ * TIME COMPLEXITY: O(n)
+ */
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums){
+            if (count == 0) candidate = num;
+            
+            count += (num == candidate) ? 1 : -1;
+        }
+        
+        return candidate;
+        
+        /** OPTIONAL VERIFICATION STEP
+         * count = 0;
+         * for (int num : nums) {
+         *  if (num == candidate){
+         *      count++;
+         *  }
+         * if (count > nums.length / 2) return candidate;
+         * else return -1;
+         * }
+         */
     }
 }
