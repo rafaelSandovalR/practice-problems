3a03fbcc2f2fe43cc56f5e0b092b125eab5d5feb
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-07-17 21:36:18 -0400

    NEW HARD PROBLEM: SUBSTRING WITH CONCATENATION OF ALL WORDS Created Hard-Practice folder. Created classes

---
+++ Hard-Practice/src/main/java/substring_with_concat/Solution.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package substring_with_concat;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public List<Integer> findSubstring(String s, String[] words){
+        
+    }
+}
