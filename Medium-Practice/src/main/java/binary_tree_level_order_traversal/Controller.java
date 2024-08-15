5e59ab1e15f9a35f5bdd82e3a9388de23771cc13
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-08-15 11:45:42 -0400

    NEW MEDIUM PROBLEM: BINARY TREE LEVEL ORDER TRAVERSAL Created classes and added outline of initial idea

---
+++ Medium-Practice/src/main/java/binary_tree_level_order_traversal/Controller.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package binary_tree_level_order_traversal;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Medium-Practice/src/main/java/binary_tree_level_order_traversal/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package binary_tree_level_order_traversal;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        // Create main list to hold sublists
+        // Create queue to hold all nodes on each level
+        // While queue is not empty
+            // Create sublist for current level
+            // Loop till size of queue to;
+                // Poll queue
+                // add value to current sublist
+                // add left child to queue
+                // add right child to queue
+        // Return main list
+    }
+}
