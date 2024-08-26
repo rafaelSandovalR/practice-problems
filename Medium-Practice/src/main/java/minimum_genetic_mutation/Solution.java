4664c321ab466fe7c9520b11722170f509dd0bb4
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-08-26 15:30:01 -0400

    MINIMUM GENETIC MUTATIONS Condensed code and removed unnecessary part. Moved check if gene is in queue to a separate method. Added comments.

--- Medium-Practice/src/main/java/minimum_genetic_mutation/Solution.java
+++ Medium-Practice/src/main/java/minimum_genetic_mutation/Solution.java
@@ -13,10 +13,10 @@ public class Solution {
         
         
         // Convert all strings to char arrays
+        int n = bank.length + 1; // number of genes including startGene
         char[] start = startGene.toCharArray();
-        final char[] end = endGene.toCharArray();
-        final char[][] bankGraph = new char[bank.length][];
-        int[] count = new int[bank.length];
+        final char[][] bankGraph = new char[n][];
+        int[] mutationCount = new int[n];
         boolean containsEnd = false; // Flag to check if end is in bank
         
         // Convert and add each bank record to 2d char array
@@ -27,53 +27,32 @@ public class Solution {
         }
         
         if (!containsEnd) return -1; // If not found, it can't be solved
-        
-        // Arrays to keep track of differences of between each bank record and start/end genes
-        int[] startDiffCount = new int[bank.length];
-        final int[] endDiffCount = new int[bank.length]; // May not need this ***
-        
-        // Populate difference count arrays
-        for (int letter = 0; letter < 8; letter++){
-            for (int bankRecord = 0; bankRecord < bank.length; bankRecord++){
-                char letterScanned = bankGraph[bankRecord][letter];
-                if (letterScanned != start[letter]) startDiffCount[letter]++;
-                if (letterScanned != end[letter]) endDiffCount[letter]++;
-
-            }
-        }
-        int[] queue = new int[bank.length];
-        int write = 0;
-        int read = 0;
-        
-        // Find the candidate genes to add to queue
-        for (int i = 0; i < startDiffCount.length; i++){
-            if (startDiffCount[i] == 1 && count[i] == 0){
-                queue[write++] = i;
-            }
-        }
-        
-        int[] prev = new int[bank.length];
                 
+        int[] queue = new int[n];
+        
+        queue[0] = 0;
+        int read = 0;
+        int write = 1;
+        
+        
+        int[] prev = new int[n]; // Holds bank index of previous gene from current
+        
         while (read != write){
-            int geneIdx = queue[read]; 
-            char[] gene = bankGraph[geneIdx];
-            count[geneIdx] = prev[read] + 1;
+            int geneIdx = queue[read];  // Current Gene Index
+            char[] gene = bankGraph[geneIdx]; // Current Gene
+            mutationCount[geneIdx] = prev[read] + 1; // Increment mutation count
             
-            if (new String(gene).equals(endGene)) return count[geneIdx];
+            // If endGene is found
+            if (new String(gene).equals(endGene)) return mutationCount[geneIdx];
             
+            // Generate array indicating how many mutations away current gene is from each bank gene
             int[] diffCount = scanDifferences(bankGraph, gene);
             
-            for (int i = 0; i < diffCount.length; i++){
-                
-                if (diffCount[i] == 0){
-                    boolean inQueue = false;
-                    for (int j = read; j < write; j++){
-                        if (queue[j] == i) inQueue = true;
-                    }
-                    
-                    if (!inQueue){
-                        queue[write++] = i;
-                    } 
+            // Loop to enqueue valid indices
+            for (int i = 0; i < n; i++){    
+                if (diffCount[i] == 0 && !inQueue(queue, i, read, write)){
+                    queue[write++] = i;
+                    prev[read] = geneIdx;
                 }
             }
             
@@ -81,6 +60,16 @@ public class Solution {
         }
         
         return -1;
+    }
+    
+    private boolean inQueue(int[] queue, int value, int read, int write){
+        boolean result = false;
+        for (int j = read; j < write; j++) {
+            if (queue[j] == value) {
+                result = true;
+            }
+        }
+        return result;
     }
     
     private int[] scanDifferences(char[][] bank, char[] gene){
@@ -94,7 +83,6 @@ public class Solution {
                 }
             }
         }
-        
         return diffCount;
     }
     
