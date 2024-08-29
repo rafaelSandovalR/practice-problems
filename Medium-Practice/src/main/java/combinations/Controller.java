5dd3f6605111df3fccce4b32d16df50b11499181
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-08-29 14:45:44 -0400

    NEW MEDIUM PROBLEM: COMBINATIONS Created classes and added initial code using recursive helper method

---
+++ Medium-Practice/src/main/java/combinations/Controller.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package combinations;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        List<List<Integer>> result = sol.combine(4, 3);
+    }
+}
---
+++ Medium-Practice/src/main/java/combinations/Solution.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public List<List<Integer>> combine(int n, int k){
+        var res = new ArrayList<List<Integer>>();
+        
+        helper(n, k, res, new ArrayList<Integer>());
+        
+        return res;
+    }
+    
+    private void helper(int n, int k, ArrayList<List<Integer>> mainList, ArrayList<Integer> subList){
+        
+        if (k == 0) {
+            mainList.add(subList);
+            return;
+        }
+        subList.add(n);
+        helper(n-1, k-1, mainList, subList);
+    }
+}
