40fe24475cdece708d602b81cb3ff3b4df943599
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-09-06 17:14:23 -0400

    CONSTRUCT QUAD TREE Overloaded construct method to pass ranges for rows and columns in order to divide into quadrants.

--- Medium-Practice/src/main/java/construct_quad_tree/Solution.java
+++ Medium-Practice/src/main/java/construct_quad_tree/Solution.java
@@ -11,11 +11,37 @@ package construct_quad_tree;
 public class Solution {
     public Node construct(int[][] grid) {
         
-        Node current = new Node();
+        return construct(grid, new int[]{0,grid.length-1}, new int[]{0, grid.length-1});
+    }
+    
+    private Node construct(int[][] grid, int[] rowRange, int[] colRange){
         
-        if (isUniform(grid)){
+        int n = rowRange[1] - rowRange[0] + 1;
+        
+        Node current = new Node();
+
+        if (isUniform(grid)) {
+            current.isLeaf = true;
+            current.val = grid[0][0] == 1;
+        } else {
+            int rowStart = rowRange[0];
+            int colStart = colRange[0];
+            int rowEnd = rowRange[1];
+            int colEnd = colRange[1];
             
+            int[] rowFirstHalf = {rowStart, rowStart + (n/2) - 1};
+            int[] rowSecondHalf = {rowStart + (n/2), rowEnd};
+            int[] colFirstHalf = {colStart, colStart + (n/2) - 1};
+            int[] colSecondHalf = {colStart + (n/2), colEnd};
+        
+            
+            current.topLeft = construct(grid, rowFirstHalf, colFirstHalf);
+            current.topRight = construct(grid, rowFirstHalf, colSecondHalf);
+            current.bottomLeft = construct(grid, rowSecondHalf, colFirstHalf);
+            current.bottomRight = construct(grid, rowSecondHalf, colSecondHalf);
         }
+        
+        return current;
     }
     
     private boolean isUniform(int[][] grid){
