ccfc1766d14ae6f37c20cf138b7dbefe08ed1b74
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-09-11 12:29:30 -0400

    MAXIMUM SUM CIRCULAR SUBARRAY Started with Kadane's algorithm and then added a loop to search for the max in reverse from the starting point of the first max subarray

--- Medium-Practice/src/main/java/maximum_sum_circular_subarray/Solution.java
+++ Medium-Practice/src/main/java/maximum_sum_circular_subarray/Solution.java
@@ -10,6 +10,43 @@ package maximum_sum_circular_subarray;
  */
 public class Solution {
     public int maxSubarraySumCircular(int[] nums){
+        int n = nums.length;
+        int current = nums[0];
+        int max = nums[0];
         
+        int start = 0;
+        int maxEnd = 0;
+        int maxStart = 0;
+        
+        for (int i = 1; i < n; i++){
+            
+            if (current + nums[i] > nums[i]){
+                current += nums[i];
+            } else {
+                current = nums[i];
+                start = i;
+            }
+            
+            if (current > max){
+                max = current;
+                maxStart = start;
+                maxEnd = i;
+            }
+            
+        }
+        
+        int prev = (maxStart - 1 + n) % n;
+        int max2 = 0;
+        current = 0;
+        
+        for (int i = prev; i != maxEnd;){
+            current += nums[i];
+            max2 = Math.max(current, max2);
+            i--;
+            i += n;
+            i %= n;
+        }
+        
+        return Math.max(max, max + max2);
     }
 }
