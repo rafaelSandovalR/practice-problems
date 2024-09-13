0e3e1274e0b4106252fe24f4cec0ed75db723f0e
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-09-13 13:08:31 -0400

    MAXIMUM SUM CIRCULAR SUBARRAY Fixed return statement to subtract min from totalSUm instead of max. Added comments

--- Medium-Practice/src/main/java/maximum_sum_circular_subarray/Solution2.java
+++ Medium-Practice/src/main/java/maximum_sum_circular_subarray/Solution2.java
@@ -7,6 +7,8 @@ package maximum_sum_circular_subarray;
 /**
  *
  * @author Rsand
+ * 
+ * Solution using minimum subarray calculation to find wrap-around sum
  */
 public class Solution2 {
     public int maxSubarraySumCircular(int[] nums){
@@ -19,9 +21,11 @@ public class Solution2 {
         
         int totalSum = nums[0];
         
+        // Iterate through the array from the second element
         for (int i = 1; i < n; i++){
-            totalSum += nums[i];
+            totalSum += nums[i]; // Update running sum
             
+            // Update the current max/min sums: either start a new subarray at the current element or extend the existing ones
             currentMax = Math.max(nums[i], currentMax + nums[i]);
             currentMin = Math.min(nums[i], currentMin + nums[i]);
             
@@ -31,6 +35,8 @@ public class Solution2 {
             
         }
         
-        return totalSum == min ? max : Math.max(max - min, max);
+        // If the total sum is equal to the minimum sum, it means all elements are negative. In this case, return the maximum sum
+        // Otherwise, the maximum circular subarray sum is either the maximum subarray sum or the total sum minus the minimum subarray sum
+        return totalSum == min ? max : Math.max(totalSum - min, max);
     }
 }
