916627c5d065f9b8078b57dae74c25d15eccda5b
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-09-24 10:25:02 -0400

    FIND K PAIRS WITH SMALLEST SUM
    Added array to keep track of index in nums2 for each element in nums1
    Changed while loop condition to when result list < k

--- Medium-Practice/src/main/java/find_k_pairs_with_smallest_sum/Solution.java
+++ Medium-Practice/src/main/java/find_k_pairs_with_smallest_sum/Solution.java
@@ -13,13 +13,16 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k){
+        var res = new ArrayList<List<Integer>>();
+        
+        // Handle edge cases
+
         int n1 = nums1.length;
         int n2 = nums2.length;
         
-        var res = new ArrayList<List<Integer>>();
-        int count = 1;
-        int left1st = 0, left2nd = 1;
-        int right1st = 0, right2nd = 1;
+        // Keeps trakc of the current index in 'nums2' for each element in 'nums1'
+        int[] arr = new int[n1];
+        arr[0] = 1; // Mark the first element in 'nums1' as used
         
         // Add the first of both lists
         var first = new ArrayList<Integer>();
@@ -27,7 +30,7 @@ public class Solution {
         first.add(nums2[0]);
         res.add(first);
         
-        while (count < k){
+        while (res.size() < k){
             var subList = new ArrayList<Integer>();
             
             int leftA = nums1[left1st], leftB = nums2[left2nd];
