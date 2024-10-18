25cb13858798c14faf3cce619ec8bcf310469470
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-10-18 09:28:46 -0400

    3SUM Added logic to check the last three indices after the main while loop. Added helper method to check if a sublist is the same as the previous.

--- Medium-Practice/src/main/java/three_sum/Solution.java
+++ Medium-Practice/src/main/java/three_sum/Solution.java
@@ -18,8 +18,9 @@ public class Solution {
         Arrays.sort(nums);
         int left = 0;
         int right = nums.length-1;
-        
         int mid = nums[left] + nums[right] < 0 ? right - 1 : 1;
+        
+        List<Integer> prev = new ArrayList<>();
         
         while (mid - 1 > left || mid + 1 < right){
             
@@ -33,6 +34,7 @@ public class Solution {
                 sum = nums[left] + nums[--mid] + nums[right];
             }
             
+            
             if (sum < 0) left++;
             else if (sum > 0) right--;
             else {
@@ -40,15 +42,41 @@ public class Solution {
                 sub.add(nums[left]);
                 sub.add(nums[mid]);
                 sub.add(nums[right]);
-                list.add(sub);
+                
+                if (different(prev, sub)) list.add(sub);
+                
+                prev = sub;
                 
                 if (mid - left < right - mid) right--;
                 else left++;
             }
             
-            
+            mid = nums[left] + nums[right] < 0 ? right - 1 : left + 1;
         }
         
+        int sum = nums[left] + nums[mid] + nums[right];
+        if (sum == 0){
+            List<Integer> remaining = new ArrayList<>();
+            remaining.add(nums[left]);
+            remaining.add(nums[mid]);
+            remaining.add(nums[right]);
+            
+            if (different(prev, remaining))
+                list.add(remaining);
+        }
+
+        
+
+        
         return list;
+    }
+    
+    private boolean different(List<Integer> prev, List<Integer> curr){
+        if (prev.size() == 0) return true;
+        for (int i = 0; i < 3; i++){
+            if (prev.get(i) != curr.get(i)) return true;
+        }
+        
+        return false;
     }
 }
