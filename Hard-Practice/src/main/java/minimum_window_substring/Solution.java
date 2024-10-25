5608f434230960d7b14932b81d91c7b526bbc50a
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-10-25 17:00:28 -0400

    MINIMUM WINDOW SUBSTRING Added logic for  valid-character overflow, this solution works for some inputs, but not all. BUG: When overflow happens, valid-characters (before first index of overflow char) get truncated.

--- Hard-Practice/src/main/java/minimum_window_substring/Solution.java
+++ Hard-Practice/src/main/java/minimum_window_substring/Solution.java
@@ -12,7 +12,6 @@ public class Solution {
     public String minWindow(String s, String t){
         String minSubString = "";
         int minLen = Integer.MAX_VALUE;
-        int start = 0, end = 0;
         
         int[] count = new int[128];
         int[] foundCount = new int[128];
@@ -28,9 +27,10 @@ public class Solution {
         }
         
         // Sliding window loop
-        for (int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
+        for (int start = -1, end = 0; end < s.length(); end++){
+            char c = s.charAt(end);
             if (count[c] > 0){
+                if (start == -1) start = end;
                 foundCount[c]++; // Update found
                 
                 // Increment count of fully accounted for characters
@@ -38,12 +38,35 @@ public class Solution {
                     charFound++;
                 } else if (foundCount[c] > count[c]){ // If too many of 'c'
 
+                    // Scan from start to end 
+                    for (int next = start; next <= end; next++){
+                        char curr = s.charAt(next);
+                        // If valid character, decrement from foundCount
+                        if (count[curr] >= 1){
+                            // If decrementing from a completed character, update charFound
+                            if (foundCount[curr] == count[curr]) charFound--;
+                            
+                            foundCount[curr]--;
+
+                        }
+                        
+                        // When character that caused overflow found, start substring from there
+                        if (curr == c){
+                            curr = s.charAt(++next);
+                            while(count[curr] < 1){
+                                curr = s.charAt(++next);
+                            }
+                            start = next;
+                            break;
+                        }
+                    }
                     
                 }
                 
                 // If all characters found and current substring length is less than minLen
                 if (charFound == charCount && end - start < minLen){
                     minSubString = s.substring(start, end + 1); // Update result string
+                    minLen = end - start;
                 }
             }
         }
