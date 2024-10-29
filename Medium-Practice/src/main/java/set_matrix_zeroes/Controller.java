c4a027c9c7bc9f88efb717139b1e391e2f8aa15c
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-10-29 17:19:10 -0400

    NEW MEDIUM PROBLEM: SET MATRIX ZEROES Created classes and solved solution using two boolean arrays and a two-pass algorithm. Where the first pass is to flag each row and column containing a zero and the second pass is to set rows/cols to zeroes

---
+++ Medium-Practice/src/main/java/set_matrix_zeroes/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package set_matrix_zeroes;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    
+}
---
+++ Medium-Practice/src/main/java/set_matrix_zeroes/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package set_matrix_zeroes;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public void setZeroes(int[][] matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+        
+        for(int r = 0; r < m; r++){
+            for(int c = 0; c < n; c++){
+               if (matrix[r][c] == 0){
+                   rows[r] = true;
+                   cols[c] = true;
+               }
+            }
+        }
+        
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (rows[r] || cols[c]) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+}
