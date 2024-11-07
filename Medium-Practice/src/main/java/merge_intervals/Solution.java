cac6fb233c0e8b7d42b2f00cdc819f00548aa3c9
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-07 14:43:47 -0500

    REVISIT: MERGE INTERVAL Added comments

--- Medium-Practice/src/main/java/merge_intervals/Solution.java
+++ Medium-Practice/src/main/java/merge_intervals/Solution.java
@@ -25,6 +25,26 @@ public class Solution {
         }
         
         // Array to track the max ending pointed enountered for each starting point
+        // Indices represents starting points of each interval, adjusted so that index 0 is the smallest starting point
+        // range[i] represents ending points of each interval; If zero, then no range starts at that point
+        /*
+            Intervals = {{3,4}, {8,9}, {2,10}, {12,20}, {21,24}};
+            Index   Value
+            0       8       ->  {2,10} since smallest value is 2, all representations are their actual value - 2
+            1       2       ->  {3,4}
+            2       0       
+            3       0
+            4       0       -> Any with zero means there is no interval with that starting point (No interval starts with 4+2=6)
+            5       0
+            6       7       ->  {8,9}
+            7       0
+            8       0
+            9       0       
+            10      18      ->  {12,22} Since ending point is 18 which is > the greatest ending value before it, then this is the
+            ...                         start of a new range. Save previous range: {Index 0 + Min 2, Value 8 + Min 2} -> {2,10}
+            ...                         
+            ...                         New range starting point is 10
+        */
         int[] range = new int[max - min + 1];
         for (int i = 0; i < intervals.length; i++){
             // Records the furthest ending point encountered so far
