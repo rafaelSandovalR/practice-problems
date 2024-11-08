07f6c717eb8be288c0530fbcb0e54fded1788bad
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-08 11:44:59 -0500

    MINIMUM NUMBER OF ARROWS TO BURST BALLOONS Added variables and first loop to build range array

--- Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
+++ Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
@@ -13,11 +13,36 @@ public class Solution {
         int res = 0;
         
         // Find Min and Max out of all intervals
+        
         // Build array of size Max - Min + 1
         //      - array indices represent start of intervals
         //      - array values represent end of intervals
+
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int[] range = new int[max - min + 1];
+        
+        for (int i = 0; i < points.length; i++){
+            int intrvlStart = points[i][0];
+            int intrvlEnd = points[i][1];
+            
+            min = Math.min(min, intrvlStart);
+            max = Math.max(max, intrvlEnd);
+            
+            range[intrvlStart - min] = intrvlEnd - min;
+        } 
+        
+        
+
+        
+        
+        
         // Using first interval, skip to array[value]
         //      - update count
         // Loop through entire array
+        for (int i = 0; i < range.length; i++){
+            
+        }
     }
 }
