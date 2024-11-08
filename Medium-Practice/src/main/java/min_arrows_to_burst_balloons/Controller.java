8d81d260a3cebd6a1a9cf51d0179e088e18c2980
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-07 23:17:35 -0500

    NEW MEDIUM PROBLEM: MINIMUM NUMBER OF ARROWS TO BURST BALLOONS Created classes and added code outline

---
+++ Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Controller.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package min_arrows_to_burst_balloons;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Controller {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package min_arrows_to_burst_balloons;
+
+/**
+ *
+ * @author Rsand
+ */
+public class Solution {
+    public int findMinArrowShots(int[][] points){
+        int res = 0;
+        
+        // Find Min and Max out of all intervals
+        // Build array of size Max - Min + 1
+        //      - array indices represent start of intervals
+        //      - array values represent end of intervals
+        // Using first interval, skip to array[value]
+        //      - update count
+        // Loop through entire array
+    }
+}
