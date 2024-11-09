5797bcdd2c9d2c48492e941541811d84e338261c
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-08 22:25:56 -0500

    MINIMUM NUMBER OF ARROWS TO BURST BALLOONS Added loop to build array and loop to count arrows. PROBLEM FOUND: Memory issue when min and max are large numbers

--- Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
+++ Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
@@ -10,39 +10,43 @@ package min_arrows_to_burst_balloons;
  */
 public class Solution {
     public int findMinArrowShots(int[][] points){
+        if (points.length == 1) return 1;
         int res = 0;
         
         // Find Min and Max out of all intervals
         
-        // Build array of size Max - Min + 1
-        //      - array indices represent start of intervals
-        //      - array values represent end of intervals
-
 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        int[] range = new int[max - min + 1];
         
         for (int i = 0; i < points.length; i++){
             int intrvlStart = points[i][0];
             int intrvlEnd = points[i][1];
-            
             min = Math.min(min, intrvlStart);
             max = Math.max(max, intrvlEnd);
-            
-            range[intrvlStart - min] = intrvlEnd - min;
         } 
         
-        
+        // Build array of size Max - Min + 1
+        //      - array indices represent start of intervals
+        //      - array values represent end of intervals
+        int[] range = new int[max - min + 1];
 
-        
-        
+        for (int i = 0; i < points.length; i++) {
+            int intrvlStart = points[i][0];
+            int intrvlEnd = points[i][1];
+
+            range[intrvlStart - min] = intrvlEnd - min;
+        }
         
         // Using first interval, skip to array[value]
         //      - update count
         // Loop through entire array
         for (int i = 0; i < range.length; i++){
-            
+            if(range[i] == 0) continue;
+            res++;
+            i = range[i];
         }
+        
+        return res;
     }
 }
