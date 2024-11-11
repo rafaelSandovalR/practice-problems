25731cf6764cd7fb0b757cbdd02c2fd6a7cabad9
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-11 16:20:04 -0500

    MINIMUM NUMBER OF ARROWS TO BURST BALLOONS Added alternate slightly more concise solution

--- Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
+++ Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
@@ -35,7 +35,24 @@ public class Solution {
     }
     
     
-    public int findMinArrowShots(int[][] points){
+    public int findMinArrowShots2(int[][] points){
+        if (points == null || points.length == 0) return 0;
         
+        // Sorting using Integer class instead of Comparator
+        Arrays.sort(points, (a,b) -> Integer.compare(a[1], b[1]));
+        
+        int arrows = 1;
+        int lastArrowPos = points[0][1];
+        
+        // Loop starting at second balloon
+        for (int i = 1; i < points.length; i++){
+            // If current balloon is out of range of arrow
+            if (points[i][0] > lastArrowPos){
+                arrows++; // Get new arrow
+                lastArrowPos = points[i][1]; // Set its position as far right with current balloon
+            }
+        }
+        
+        return arrows;
     }
 }
