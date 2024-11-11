10f804d454bb01de35c5cd2003a1a5042597d142
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-11 15:32:09 -0500

    MINIMUM NUMBER OF ARROWS TO BURST BALLOONS Added sorting of points, by their second value (end points). Rewrote loop logic by adding a nested loop to determine how far the arrow would reach popping balloons.

--- Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
+++ Medium-Practice/src/main/java/min_arrows_to_burst_balloons/Solution.java
@@ -4,6 +4,9 @@
  */
 package min_arrows_to_burst_balloons;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  *
  * @author Rsand
@@ -13,40 +16,21 @@ public class Solution {
         if (points.length == 1) return 1;
         int res = 0;
         
-        // Find Min and Max out of all intervals
         
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
         
         for (int i = 0; i < points.length; i++){
-            int intrvlStart = points[i][0];
-            int intrvlEnd = points[i][1];
-            min = Math.min(min, intrvlStart);
-            max = Math.max(max, intrvlEnd);
-        } 
-        
-        // Build array of size Max - Min + 1
-        //      - array indices represent start of intervals
-        //      - array values represent end of intervals
-        int[] range = new int[max - min + 1];
 
-        for (int i = 0; i < points.length; i++) {
-            int intrvlStart = points[i][0];
-            int intrvlEnd = points[i][1];
-
-            range[intrvlStart - min] = intrvlEnd - min;
-        }
-        
-        // Using first interval, skip to array[value]
-        //      - update count
-        // Loop through entire array
-        for (int i = 0; i < range.length; i++){
-            if(range[i] == 0) continue;
             res++;
-            i = range[i];
+            
+            int arrow = points[i][1];
+            
+            while (i+1 < points.length && points[i+1][0] <= arrow){
+                i++;
+            }
         }
-        
+
         return res;
     }
+    
 }
