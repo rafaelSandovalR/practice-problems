e7b4b84bf1d4a523bdc6accdd4171c16290372a1
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-12 10:29:51 -0500

    EVALUATE REVERSE POLISH NOTATION Added comments

--- Medium-Practice/src/main/java/evaluate_reverse_polish_notation/Solution.java
+++ Medium-Practice/src/main/java/evaluate_reverse_polish_notation/Solution.java
@@ -10,12 +10,15 @@ package evaluate_reverse_polish_notation;
  */
 public class Solution {
     public int evalRPN(String[] tokens){
-        int[] stack = new int[tokens.length];
-        int top = -1;
-        int a = 0, b = 0;
+        int[] stack = new int[tokens.length]; // Int array representing a stack
+        int top = -1; // Pointer indicating top of stack
+        int a = 0, b = 0; // Operands
         
+        // Iterate through all tokens
         for (String token : tokens){
             switch(token){
+                // When token is an operator, 'b' is at the top of the stack 
+                // followed by 'a'. Important for subtraction and division
                 case "+" : 
                     b = stack[top--];
                     a = stack[top--];
@@ -37,6 +40,7 @@ public class Solution {
                     stack[++top] = Integer.valueOf(a / b);
                     break;
                 default:
+                    // If token is a numerical value, simply add it to the stack
                     stack[++top] = Integer.valueOf(token);   
             }
         }
