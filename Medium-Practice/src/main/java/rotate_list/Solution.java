9746584ebef66a23588344841e53d1c87f88ce93
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-15 09:34:51 -0500

    ROTATE LIST Added comments

--- Medium-Practice/src/main/java/rotate_list/Solution.java
+++ Medium-Practice/src/main/java/rotate_list/Solution.java
@@ -10,24 +10,30 @@ package rotate_list;
  */
 public class Solution {
     public ListNode rotateRight(ListNode head, int k){
-        if (head == null) return null;
+        if (head == null) return null; // Edge case: empty input
+        
+        // Two pointers: 'first' will find the kth node, 'second' will find the new tail
         ListNode first = head;
         ListNode second = head;
         
         for (int i = 0; i < k; i++){
+            // Iterate through linked list. If end is reached, wrap back around
             first = first.next == null ? head : first.next;
         }
         
+        // If kth node is head, then no rotation is needed
         if (first == head) return head;
         
+        // Get 'first' to last node, 'second' will lag k steps behind
         while (first.next != null){
             first = first.next;
             second = second.next;
         }
         
-        ListNode rotatedHead = second.next;
-        second.next = null;
-        first.next = head;
+        // Here is where the new split will happen
+        ListNode rotatedHead = second.next; // New head is node after 'second'
+        second.next = null; // 'second' is new tail
+        first.next = head; // connect previous tail to previous head
         
         return rotatedHead;  
     }
