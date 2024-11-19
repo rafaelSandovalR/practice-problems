34cb189648cb3a63d3adf8c93503fe9f9ba4978f
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-19 12:11:08 -0500

    POPULATING NEXT RIGHT POINTERS IN EACH NODE II Added helper method to set all 'next' pointers to their parent node. Adjusted getRight logic

--- Medium-Practice/src/main/java/populating_next_right_pointers/Solution.java
+++ Medium-Practice/src/main/java/populating_next_right_pointers/Solution.java
@@ -9,42 +9,62 @@ package populating_next_right_pointers;
  * @author Rsand
  */
 public class Solution {
-
     public Node connect(Node root){
 
+        prepNext(root);
         traverse(root);
         return root;
     }
     
+    // Method to set all 'next' pointers to their parent node
+    private void prepNext(Node root){
+        if (root == null) return;
+        if (root.left != null) root.left.next = root;
+        if (root.right != null) root.right.next = root;
+        
+        prepNext(root.left);
+        prepNext(root.right);
+    }
+    
     private void traverse(Node root){
+        // Variables to simplify following code
         Node left = root.left;
         Node right = root.right;
         
-        if (left != null) root.left.next = root;
-        if (right != null) root.right.next = root;
-        
-        if (left != null && right != null){ // Case 1: Root has two children
+        // Case 1: Root has two children
+        if (left != null && right != null){ 
             left.next = right;
             right.next = getRight(right.next, right);
-        } else if (left != null){ // Case 2: Root only has left child
+        } 
+        // Case 2: Root only has left child
+        else if (left != null){ 
             left.next = getRight(left.next, left);
-        } else if (right != null){ // Case 3: Root only has right child
+        } 
+        // Case 3: Root only has right child
+        else if (right != null){ 
             right.next = getRight(right.next, right);
-        } else {
-            return; // Case 4: Root has no children
+        } 
+        // Case 4: Root has no children
+        else {
+            return; 
         }
         
+        // Traverse left and right subtrees
         if (left != null) traverse(left);
         if (right != null) traverse(right);
-        
     }
     
     private Node getRight(Node next, Node curr){
+        
+        
+        if (next.left == curr && next.right != null) return getRight(next.right, next);
+       
         
         if (next.right == curr || next.right == null){
             if (next.next == null) return null;
             return getRight(next.next, next);
         }
+        
         
         if (next.left != null) return next.left;
         
