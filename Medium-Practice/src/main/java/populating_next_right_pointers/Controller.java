06907260d3e9bf3fddc86ca57a50808ceb085f8c
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2024-11-20 12:03:07 -0500

    POPULATING NEXT RIGHT POINTERS IN EACH NODE II Added test code

--- Medium-Practice/src/main/java/populating_next_right_pointers/Controller.java
+++ Medium-Practice/src/main/java/populating_next_right_pointers/Controller.java
@@ -4,6 +4,7 @@
  */
 package populating_next_right_pointers;
 
+
 /**
  *
  * @author Rsand
@@ -11,6 +12,28 @@ package populating_next_right_pointers;
 public class Controller {
     public static void main(String[] args) {
         
+        
         // [2,1,3,0,7,9,1,2,null,1,0,null,null,8,8,null,null,null,null,7]
+//        Node n15 = new Node(5);
+//        Node n14 = new Node(5);
+        Node n13 = new Node(7);
+        Node n12 = new Node(8);
+        Node n11 = new Node(8);
+        Node n10 = new Node(0,n13,null,null);
+        Node n9 = new Node(1);
+        Node n8 = new Node(2);
+        Node n7 = new Node(1,n11,n12,null);
+        Node n6 = new Node(9);
+        Node n5 = new Node(7,n9,n10,null);
+        Node n4 = new Node(0,n8,null,null);
+        Node n3 = new Node(3,n6,n7,null);
+        Node n2 = new Node(1,n4,n5,null);
+        Node root = new Node(2,n2,n3,null);
+        
+
+        Solution sol = new Solution();
+        Node connected = sol.connect(root);
+        
+        
     }
 }
